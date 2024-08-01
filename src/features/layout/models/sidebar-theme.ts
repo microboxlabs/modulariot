77759ddd6dae8c3b73a96985dd1dde83a6672dd6
Michel David <mdavid.cu@gmail.com>
2024-08-01 14:39:35 -0400

    fix: adjust desktop sidebar border color in dark mode

--- src/features/layout/models/sidebar-theme.ts
+++ src/features/layout/models/sidebar-theme.ts
@@ -3,6 +3,6 @@ import { CustomFlowbiteTheme } from "flowbite-react";
 export const sideBarTheme: CustomFlowbiteTheme["sidebar"] = {
   root: {
     inner:
-      "h-full overflow-y-auto border-r overflow-x-hidden px-3 py-4 dark:bg-gray-800 border-gray-200",
+      "h-full overflow-y-auto border-r overflow-x-hidden px-3 py-4 border-gray-200 dark:border-gray-900 dark:bg-gray-800 ",
   },
 };

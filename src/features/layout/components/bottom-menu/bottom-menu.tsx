5508c027470fdb27a9dbd3bb0b76e13c1417d47a
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2024-09-11 14:13:50 -0300

    Implement designed bottom menu in sidebar

--- src/features/layout/components/bottom-menu/bottom-menu.tsx
+++ src/features/layout/components/bottom-menu/bottom-menu.tsx
@@ -1,11 +1,30 @@
 import { twMerge } from "tailwind-merge";
 import { HiAdjustments, HiCog } from "react-icons/hi";
-import { Tooltip } from "flowbite-react";
+import { Sidebar, Tooltip } from "flowbite-react";
 import Link from "next/link";
+import LifeSaver from "./LifeSaver";
 
 export default function BottomMenu({ isCollapsed }: { isCollapsed: boolean }) {
   return (
     <div
+      className={twMerge(
+        "flex items-center justify-center gap-4 mb-9",
+        isCollapsed && "flex-col",
+      )}
+    >
+      <Sidebar.ItemGroup>
+        <Sidebar.Item href="#" icon={HiCog}>
+          <Link href="/users/settings">Ajustes</Link>
+        </Sidebar.Item>
+        <Sidebar.Item href="#" icon={LifeSaver}>
+          Ayuda y cómo empezar
+        </Sidebar.Item>
+      </Sidebar.ItemGroup>
+    </div>
+  );
+}
+
+/* <div
       className={twMerge(
         "flex items-center justify-center gap-4 mb-9",
         isCollapsed && "flex-col",
@@ -24,7 +43,5 @@ export default function BottomMenu({ isCollapsed }: { isCollapsed: boolean }) {
           <HiCog className="h-6 w-6" />
         </Link>
       </Tooltip>
-      <div>{/* <LanguageDropdown /> */}</div>
-    </div>
-  );
-}
+      <div>{/* <LanguageDropdown /> /}</div>
+      </div> */

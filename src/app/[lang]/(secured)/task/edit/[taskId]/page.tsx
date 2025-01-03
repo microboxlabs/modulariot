50b7e2870346d10f75df1ae7fd2fd3b2e94939b3
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-01-03 14:39:36 -0300

    general task visor

--- src/app/[lang]/(secured)/task/edit/[taskId]/page.tsx
+++ src/app/[lang]/(secured)/task/edit/[taskId]/page.tsx
@@ -1,7 +1,10 @@
 import { ParamsWithLang } from "@/features/i18n/i18n.service.types";
 import { TaskEditPageParams } from "./page.types";
 import { auth } from "@/auth";
-import { getTaskById } from "@/features/common/providers/alfresco-api/alfresco-api.provider";
+import {
+  getFinishedWorkflows,
+  getTaskById,
+} from "@/features/common/providers/alfresco-api/alfresco-api.provider";
 import { getDictionary } from "@/features/i18n/i18n.service";
 import { TaskForm } from "@/features/task-forms/components/task-form/task-form";
 import { notFound } from "next/navigation";
@@ -13,7 +16,23 @@ export default async function TaskEditPage({
   const session = await auth();
   const [, _dictionary] = await getDictionary(lang);
   try {
-    const task = await getTaskById(session!.user.ticket, taskId);
+    // Get both results upfront
+    const taskResult = await getTaskById(session!.user.ticket, taskId);
+    let task = taskResult;
+    if (task == "null") {
+      const finishedWorkflows = await getFinishedWorkflows(
+        session!.user.ticket,
+        {
+          from: 0,
+          size: 50,
+          definitionKey: "shippingCoordinatorProcess",
+        },
+      ).then((res) => ({
+        tasks: res.workflows,
+        total: res.total,
+      }));
+      task = finishedWorkflows.tasks.find((t) => t.id === taskId);
+    }
     return <TaskForm task={task} lang={lang} ticket={session!.user.ticket} />;
   } catch (e: any) {
     if (e?.status === 401) {
@@ -21,5 +40,4 @@ export default async function TaskEditPage({
     }
     notFound();
   }
-  return null;
 }
--- src/features/task-forms/components/task-form/task-form.tsx
+++ src/features/task-forms/components/task-form/task-form.tsx
@@ -6,6 +6,9 @@ import {
   TYPE_WFSHIP_OVERLORD_TRIP_INIT_TASK,
   TYPE_WFSHIP_SOVOS_DIGITAL_SIGNATURE,
   TYPE_WFSHIP_TRIP_OUTSIDE_INITIATED_TASK,
+  OUTCOME_TRIP_INITIATED,
+  OUTCOME_TRIP_CANCELED,
+  OUTCOME_TRIP_ANNULLED,
 } from "../../services/form.service";
 import TransportValidationForm from "../transport-validation-form/transport-validation-form";
 import { TaskFormProps } from "./task-form.types";
@@ -17,12 +20,64 @@ import ShippingDetailsTaskForm from "../shipping-details-task-form/shipping-deta
 import SovosVerificationForm from "../sovos-verification-form/sovos-verification-form";
 import { getUserProfile } from "@/features/common/providers/alfresco-api/alfresco-api.provider";
 import MissionControlTripInitForm from "../mission-control-trip-init-form/mission-control-trip-init-form";
+import {
+  CancelledTripView,
+  CompletedTripView,
+  GeneralTripView,
+  VoidedTripView,
+} from "@/features/shipping/components/historical-task-views";
 
 export async function TaskForm({ task, lang, ticket }: TaskFormProps) {
   const [_dict, dictionary] = await getDictionary(lang ?? defaultLocale);
   const userInstance = await getUserProfile(ticket!);
   const user = JSON.stringify(userInstance);
-  switch (task.taskFormKey) {
+  console.log("task", task);
+
+  // Handle historical tasks
+  if (task?.persistentState?.endTime) {
+    switch (task.taskFormKey) {
+      case OUTCOME_TRIP_INITIATED: //TYPE_WFSHIP_COMPLETED_TRIP:
+        return (
+          <CompletedTripView
+            lang={lang ?? defaultLocale}
+            task={task}
+            user={user}
+            msg={(dictionary.pages as I18nRecord).completedTrip as I18nRecord}
+          />
+        );
+      case OUTCOME_TRIP_CANCELED: //TYPE_WFSHIP_CANCELLED_TRIP:
+        return (
+          <CancelledTripView
+            lang={lang ?? defaultLocale}
+            task={task}
+            user={user}
+            msg={(dictionary.pages as I18nRecord).cancelledTrip as I18nRecord}
+          />
+        );
+      case OUTCOME_TRIP_ANNULLED: //TYPE_WFSHIP_VOIDED_TRIP:
+        return (
+          <VoidedTripView
+            lang={lang ?? defaultLocale}
+            task={task}
+            user={user}
+            msg={(dictionary.pages as I18nRecord).voidedTrip as I18nRecord}
+          />
+        );
+      default:
+        return (
+          <GeneralTripView
+            lang={lang ?? defaultLocale}
+            task={task}
+            user={user}
+            msg={
+              (dictionary.pages as I18nRecord).historicalTaskView as I18nRecord
+            }
+          />
+        );
+    }
+  }
+
+  switch (task?.taskFormKey) {
     case TYPE_WFSHIP_TRANSPORT_VALIDATION_TASK:
       return (
         <TransportValidationForm

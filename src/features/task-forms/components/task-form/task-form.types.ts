439f1b096184768787d24016b5fd5d6242fdde8c
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-01-06 17:31:23 -0300

    refactor on taskform component

--- src/features/task-forms/components/task-form/task-form.types.ts
+++ src/features/task-forms/components/task-form/task-form.types.ts
@@ -1,8 +1,11 @@
-import { TaskResponse } from "@/features/common/providers/alfresco-api/alfresco-api.types";
+import {
+  HistoricalWorkflow,
+  TaskResponse,
+} from "@/features/common/providers/alfresco-api/alfresco-api.types";
 import { I18nRecord } from "@/features/i18n/i18n.service.types";
 
 export type TaskFormProps = {
-  task: TaskResponse;
+  task: TaskResponse | HistoricalWorkflow;
   lang: string;
   msg?: I18nRecord;
   ticket?: string;

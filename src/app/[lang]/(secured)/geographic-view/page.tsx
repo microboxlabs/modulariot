3a914c57faedc04472dc59731c50636898f2f1bc
khalzz <fearushbclear>
2025-01-16 14:30:34 -0300

    Implementation of map view

---
+++ src/app/[lang]/(secured)/geographic-view/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css"; // for the base style of mapbox maps
+import DeckGL, { FlyToInterpolator, LayersList } from "deck.gl";
+
+// This is defined so i can then try to add a "visualization selector" if the user wants the satelital view or not
+const mapboxStyles = {
+  "streets-v9": "mapbox://styles/mapbox/streets-v9",
+  "satellite-v9": "mapbox://styles/mapbox/satellite-v9",
+  "satellite-streets-v11": "mapbox://styles/mapbox/satellite-streets-v11",
+  "dark-v10": "mapbox://styles/mapbox/dark-v10",
+  "light-v10": "mapbox://styles/mapbox/light-v10",
+  "outdoors-v11": "mapbox://styles/mapbox/outdoors-v11",
+  "hybrid-v10": "mapbox://styles/mapbox/hybrid-v10",
+};
+const INITIAL_VIEW_STATE = {
+  longitude: -70.668505,
+  latitude: -33.439764,
+  zoom: 10,
+  pitch: 0,
+  transitionDuration: 1000,
+  transitionInterpolator: new FlyToInterpolator(),
+  transitionEasing: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+};
+
+export default function GeographicViewPage() {
+  const layers: LayersList = [
+    // Here we will add the layers of information that will be displayed in front of the map
+  ];
+
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+    >
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+        initialViewState={{
+          longitude: 0.45,
+          latitude: 51.47,
+          zoom: 11,
+        }}
+        mapStyle={mapboxStyles["satellite-streets-v11"]}
+      />
+    </DeckGL>
+  );
+}

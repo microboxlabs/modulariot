d69c837e7826224fbe5750d2fe120a51179fb8ab
khalzz <fearushbclear>
2025-02-04 10:50:35 -0300

    Added table layout

--- src/features/symptoms/components/condition-icon.tsx
+++ src/features/symptoms/components/condition-icon.tsx
@@ -1,9 +1,17 @@
 import { Conditions } from "./table-item.type";
+import Image from "next/image";
 
 export default function ConditionIcon({ condition }: { condition: string }) {
   return (
     <div
-      className={`gap-2 h-10 w-10 ${Conditions[condition as keyof typeof Conditions].innerColor} ${Conditions[condition as keyof typeof Conditions].color} border-2 rounded-full m-auto`}
-    ></div>
+      className={`gap-2 h-10 w-10 display-flex justify-center items-center ${Conditions[condition as keyof typeof Conditions].innerColor} ${Conditions[condition as keyof typeof Conditions].color} border-2 rounded-full`}
+    >
+      <Image
+        src={Conditions[condition as keyof typeof Conditions].icon}
+        alt={condition}
+        width={100}
+        height={100}
+      />
+    </div>
   );
 }
--- src/features/symptoms/table.tsx
+++ src/features/symptoms/table.tsx
@@ -1,4 +1,12 @@
-import { Table, TableHead, TableHeadCell, TableBody } from "flowbite-react";
+"use client";
+
+import {
+  Table,
+  TableHead,
+  TableHeadCell,
+  TableBody,
+  Pagination,
+} from "flowbite-react";
 import TableItem from "./components/table-item";
 
 // Condition can be:
@@ -28,28 +36,89 @@ const data = [
     service: "V1406865",
     alertType: "Conducción máxima continua",
   },
+  {
+    condition: "treatment",
+    licensePlate: "XX BB 21",
+    time: "30 seg.",
+    trip: "STG-ANF",
+    driver: "ANONIMO ANDRÉS",
+    date: "2025-01-01 12:00:00",
+    service: "V1406865",
+    alertType: "Conducción máxima continua",
+  },
+  {
+    condition: "stable",
+    licensePlate: "XX BB 21",
+    time: "30 seg.",
+    trip: "STG-ANF",
+    driver: "ANONIMO ANDRÉS",
+    date: "2025-01-01 12:00:00",
+    service: "V1406865",
+    alertType: "Conducción máxima continua",
+  },
+  {
+    condition: "compromised",
+    licensePlate: "XX BB 21",
+    time: "30 seg.",
+    trip: "STG-ANF",
+    driver: "ANONIMO ANDRÉS",
+    date: "2025-01-01 12:00:00",
+    service: "V1406865",
+    alertType: "Conducción máxima continua",
+  },
+  {
+    condition: "observation",
+    licensePlate: "XX BB 21",
+    time: "30 seg.",
+    trip: "STG-ANF",
+    driver: "ANONIMO ANDRÉS",
+    date: "2025-01-01 12:00:00",
+    service: "V1406865",
+    alertType: "Conducción máxima continua",
+  },
+  {
+    condition: "remission",
+    licensePlate: "XX BB 21",
+    time: "30 seg.",
+    trip: "STG-ANF",
+    driver: "ANONIMO ANDRÉS",
+    date: "2025-01-01 12:00:00",
+    service: "V1406865",
+    alertType: "Conducción máxima continua",
+  },
 ];
 
 export default function SymptomsTable() {
   return (
     <div className="flex flex-col gap-4 w-full">
       <div className="text-2xl font-bold">Condiciones</div>
-      <Table striped>
-        <TableHead>
-          <TableHeadCell>Condición</TableHeadCell>
-          <TableHeadCell>Tiempo Activo</TableHeadCell>
-          <TableHeadCell>viaje</TableHeadCell>
-          <TableHeadCell>Conductor</TableHeadCell>
-          <TableHeadCell>Fecha de salida</TableHeadCell>
-          <TableHeadCell>Servicio</TableHeadCell>
-          <TableHeadCell>Tipo de alerta</TableHeadCell>
-        </TableHead>
-        <TableBody className="divide-y">
-          {data.map((item) => (
-            <TableItem key={item.condition} data={item} />
-          ))}
-        </TableBody>
-      </Table>
+      <div className="shadow-md rounded-lg w-full h-fit">
+        <Table striped className="w-full">
+          <TableHead>
+            <TableHeadCell>Condición</TableHeadCell>
+            <TableHeadCell>Tiempo Activo</TableHeadCell>
+            <TableHeadCell>viaje</TableHeadCell>
+            <TableHeadCell>Conductor</TableHeadCell>
+            <TableHeadCell>Fecha de salida</TableHeadCell>
+            <TableHeadCell>Servicio</TableHeadCell>
+            <TableHeadCell>Tipo de alerta</TableHeadCell>
+          </TableHead>
+          <TableBody className="divide-y">
+            {data.map((item) => (
+              <TableItem key={item.condition} data={item} />
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex overflow-x-auto sm:justify-center">
+        <Pagination
+          currentPage={1}
+          totalPages={100}
+          onPageChange={() => {
+            console.log("ejemplo");
+          }}
+        />
+      </div>
     </div>
   );
 }

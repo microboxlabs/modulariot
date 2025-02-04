8544ad85931f336b78111274516d5a7c418219e3
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-02-04 15:58:33 -0300

    Refactor condition icons in symptoms cards with dynamic sizing

--- src/features/symptoms/components/condition-icon.tsx
+++ src/features/symptoms/components/condition-icon.tsx
@@ -1,10 +1,16 @@
 import { Conditions } from "./table-item.type";
 import Image from "next/image";
 
-export default function ConditionIcon({ condition }: { condition: string }) {
+export default function ConditionIcon({
+  condition,
+  size = "h-10 w-10",
+}: {
+  condition: string;
+  size?: string;
+}) {
   return (
     <div
-      className={`gap-2 h-10 w-10 display-flex justify-center items-center ${Conditions[condition as keyof typeof Conditions].innerColor} ${Conditions[condition as keyof typeof Conditions].color} border-2 rounded-full`}
+      className={`gap-2 ${size} display-flex justify-center items-center ${Conditions[condition as keyof typeof Conditions].innerColor} ${Conditions[condition as keyof typeof Conditions].color} border-2 rounded-full`}
     >
       <Image
         src={Conditions[condition as keyof typeof Conditions].icon}

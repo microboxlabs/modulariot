61b9a2753343df1bf74791acb7841c9cb160a024
Michel David <mdavid.cu@gmail.com>
2025-02-17 10:21:07 -0300

    Update breadcrumb and internationalization for geographic view and symptoms pages

--- src/app/[lang]/(secured)/symptoms/page.tsx
+++ src/app/[lang]/(secured)/symptoms/page.tsx
@@ -1,7 +1,7 @@
 import { HiClipboardList } from "react-icons/hi";
 import { Breadcrumb } from "@/features/common/components/Breadcrumb/Breadcrumb";
 import { getDictionary } from "@/features/i18n/i18n.service";
-import { ParamsWithLang } from "@/features/i18n/i18n.service.types";
+import { I18nRecord, ParamsWithLang } from "@/features/i18n/i18n.service.types";
 import ClientSymptoms from "../../../../features/symptoms/client-symptoms";
 
 export default async function SymptomsPage({
@@ -12,10 +12,10 @@ export default async function SymptomsPage({
     <div className="h-full w-full flex flex-col bg-white dark:bg-gray-900">
       <div className="px-4 pt-6 pb-2">
         <Breadcrumb
-          path={["Control Tower", "symptoms"]}
+          path={["mission_control", "symptoms"]}
           lang={lang}
           rootIcon={<HiClipboardList className="mr-2 h-4 w-4" />}
-          dict={dict}
+          dict={dict["symptoms"] as I18nRecord}
         />
       </div>
       {/* 

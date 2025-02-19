fcd7b3d38f381966ccd7f69db9987193f5035d4d
khalzz <fearushbclear>
2025-02-19 16:00:11 -0300

    build fix 3

--- src/features/symptoms/components/table-component.tsx
+++ src/features/symptoms/components/table-component.tsx
@@ -97,7 +97,7 @@ export default function SymptomsTable({
                 dict={dict}
                 compact={compact}
               />
-            ))}    
+            ))}
           </TableBody>
         </Table>
       </div>

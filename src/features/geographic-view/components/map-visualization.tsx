a1ce397cc704eab68c433e5260cf3355216a4bad
khalzz <fearushbclear>
2025-02-19 10:28:03 -0300

    Fixed sizes of table in symptoms and map base position/angles

--- src/features/geographic-view/components/map-visualization.tsx
+++ src/features/geographic-view/components/map-visualization.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-/* eslint-disable */
-
 import React, { useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css"; // for the base style of mapbox maps
-import DeckGL, { FlyToInterpolator, PickingInfo } from "deck.gl";
+import DeckGL, { FlyToInterpolator } from "deck.gl";
+import type { PickingInfo } from "@deck.gl/core";
 import { PinLayer } from "./pin_layer_clustered";
-import { HiChevronLeft } from "react-icons/hi";
 import MapButton from "./map-button";
 import SideBar from "./side-bar/side-bar";
 import { BsStars } from "react-icons/bs";
@@ -14,6 +12,7 @@ import { PulsePinLayer } from "./pulse";
 import Map from "react-map-gl";
 import { useMapPositions } from "../hooks/use-map-positions";
 import { MapPosition, MapPositionProperties } from "../types/map";
+
 // This is defined so i can then try to add a "visualization selector" if the user wants the satelital view or not
 const mapboxStyles = {
   "streets-v9": "mapbox://styles/mapbox/streets-v9",
@@ -48,9 +47,11 @@ const INITIAL_VIEW_STATE: ViewStateType = {
   longitude: -70.668505,
   latitude: -33.439764,
   zoom: 6.5,
-  pitch: 0,
-  bearing: 0,
-  transitionDuration: 1000,
+  // base rotation
+  pitch: 45,
+  bearing: 45,
+  // base rotation
+  transitionDuration: 500,
   transitionInterpolator: new FlyToInterpolator(),
   transitionEasing: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
   padding: {
@@ -62,42 +63,6 @@ const INITIAL_VIEW_STATE: ViewStateType = {
   width: 100,
   height: 100,
 };
-
-/* const GenerateRandomPositions = (numberOfPositions: number) => {
-  return Array.from({ length: numberOfPositions }, () => ({
-    longitude: Math.random() * 360 - 180,
-    latitude: Math.random() * 180 - 90,
-    rotation: Math.random() * 360,
-  }));
-};
-
-const positions = GenerateRandomPositions(1000); */
-
-/* INDIVIDUAL POSITION TEST */
-const individual_position_test = [
-  {
-    longitude: -70.668505,
-    latitude: -33.439764,
-    rotation: 0,
-  },
-];
-
-const states = ["stable", "critical", "code black", "none"];
-
-/* const generateRandomPulsarPositions = (count: number) => {
-  return Array.from({ length: count }, () => ({
-    state: states[Math.floor(Math.random() * states.length)],
-    geometry: {
-      coordinates: [
-        Math.random() * 360 - 180, // Random longitude
-        Math.random() * 180 - 90, // Random latitude
-      ],
-    },
-  }));
-};
-
-// Example usage
-const pulsar_position_test = generateRandomPulsarPositions(100000); */
 
 const stateToColor = {
   "code black": [0, 0, 0], // Black
@@ -131,6 +96,26 @@ type MapVisualizationProps = {
   specific_view?: boolean;
 };
 
+function zoom_on_pin(
+  object: any,
+  setViewState: (viewState: ViewStateType) => void,
+  viewState: ViewStateType,
+) {
+  if (object) {
+    const longitude = object.geometry.coordinates[0];
+    const latitude = object.geometry.coordinates[1];
+
+    setViewState({
+      ...viewState,
+      longitude,
+      latitude,
+      zoom: object.properties.cluster ? viewState.zoom + 2.0 : 15.0,
+      transitionDuration: 1000,
+      transitionInterpolator: new FlyToInterpolator(),
+    });
+  }
+}
+
 export default function MapVisualization({
   dict,
   specific_view = false,
@@ -156,51 +141,65 @@ export default function MapVisualization({
   }, [mapPositions]); */
 
   // Transform API data to GeoJSON format
-  const geoJson = React.useMemo(() => ({
-    type: "FeatureCollection",
-    features: mapPositions?.map((item: MapPosition) => ({
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [item.longitude, item.latitude],
-      },
-      properties: {
-        color: stateToColor[item.status as keyof typeof stateToColor] || [0, 0, 0],
-        rotation: (item.heading * (180 / Math.PI)),//item.rotation,
-        licensePlate: item.asset_id,//item.licensePlate,
-        driver: item.driver_id,//item.driver,
-        trip: item.trip_id,//item.trip,
-      },
-    })) || [],
-  }), [mapPositions]);
-  
+  const geoJson = React.useMemo(
+    () => ({
+      type: "FeatureCollection",
+      features:
+        mapPositions?.map((item: MapPosition) => ({
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [item.longitude, item.latitude],
+          },
+          properties: {
+            color: stateToColor[item.status as keyof typeof stateToColor] || [
+              0, 0, 0,
+            ],
+            rotation: item.heading * (180 / Math.PI),
+            licensePlate: item.asset_id,
+            driver: item.driver_id,
+            trip: item.trip_id,
+          },
+        })) || [],
+    }),
+    [mapPositions],
+  );
+
   const layers = !specific_view
     ? [
-      new PinLayer({
-        data: mapPositions || [],
-        zoom: viewState.zoom,
-      }),
-    ]
+        new PinLayer({
+          data: mapPositions || [],
+          zoom: viewState.zoom,
+          onClick: ({ object }: { object: any }) => {
+            zoom_on_pin(object, setViewState, viewState);
+          },
+        }),
+      ]
     : [
-      new PulsePinLayer({
-        data: geoJson,
-        rotation,
-        zoom: viewState.zoom,
-      }),
-      new PinLayer({
-        data: mapPositions ? [mapPositions[0]] : [],
-        zoom: viewState.zoom,
-      }),
-    ];
+        new PulsePinLayer({
+          data: geoJson,
+          rotation,
+          zoom: viewState.zoom,
+        }),
+        new PinLayer({
+          data: mapPositions ? [mapPositions[0]] : [],
+          zoom: viewState.zoom,
+          onClick: ({ object }: { object: any }) => {
+            zoom_on_pin(object, setViewState, viewState);
+          },
+        }),
+      ];
 
   if (loading) {
-    return <div className="h-full w-full flex items-center justify-center">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900" />
-    </div>;
+    return (
+      <div className="h-full w-full flex items-center justify-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900" />
+      </div>
+    );
   }
 
   if (error) {
-    console.error('Map error:', error);
+    console.error("Map error:", error);
     // Continue rendering with empty data instead of showing error
   }
 
@@ -210,15 +209,17 @@ export default function MapVisualization({
         viewState={viewState}
         controller={true}
         layers={layers}
-        onViewStateChange={({ viewState }) =>
+        onViewStateChange={({ viewState }: { viewState: ViewStateType }) =>
           setViewState(viewState as ViewStateType)
         }
-        getTooltip={({object}: PickingInfo<MapPositionProperties>) => {
+        getTooltip={({ object }: PickingInfo<MapPositionProperties>) => {
           if (object) {
             if (object.properties.cluster) {
               return null;
             }
-            return { text: `Patente: ${object.properties.asset_id}\n Servicio: ${object.properties.trip_id}\n Fecha y Hora: ${new Date(object.properties.timestamp).toLocaleString()}` };
+            return {
+              text: `Patente: ${object.properties.asset_id}\n Servicio: ${object.properties.trip_id}\n Fecha y Hora: ${new Date(object.properties.timestamp).toLocaleString()}`,
+            };
           }
           return null;
         }}

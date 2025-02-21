51e1e22ec8bcf5c8204ec53c1f75637c7234fd19
khalzz <fearushbclear>
2025-02-21 10:23:47 -0300

    Added base filter component for map view

---
+++ src/features/geographic-view/components/filter-component.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import MapButton from "./map-button";
+import Image from "next/image";
+import { IoClose } from "react-icons/io5";
+import { IconType } from "react-icons";
+type Option = {
+  text: string;
+  filter_value: string;
+  icon: string;
+  activated: boolean;
+};
+
+export default function FilterComponent({
+  icon,
+  options,
+  label,
+  icon_size = "w-5 h-5",
+}: {
+  icon: string | IconType;
+  options: Option[];
+  label: string;
+  icon_size?: string;
+}) {
+  const [expanded, set_expanded] = useState(false);
+  const [filter_options, set_filter_options] = useState<Option[]>(options);
+
+  // Calculate the number of activated options
+  const activatedCount = filter_options.filter(
+    (option) => option.activated,
+  ).length;
+
+  return (
+    <div className="relative h-full flex flex-row gap-2">
+      <div
+        className={`relative h-10 ${
+          expanded ? "max-w-10" : "max-w-[1000px]"
+        } transition-all duration-300`}
+      >
+        <MapButton
+          main_color="bg-white dark:bg-gray-800"
+          button_color="bg-white dark:bg-gray-800"
+          icon={icon}
+          text={label}
+          icon_size={icon_size}
+          onClick={() => set_expanded(!expanded)}
+          hover_disabled={expanded}
+        />
+        <div className="absolute h-10 w-10 top-0 left-0">
+          <div
+          className={`flex flex-row items-center justify-center gap-2 absolute transition-all duration-300 right-[-6px] top-[-6px] border-2 border-white z-50 bg-amber-300 rounded-full w-5 h-5 ${
+            activatedCount > 0 ? "opacity-100" : "opacity-0"
+          }`}
+          >
+            <p className="text-gray-900 text-xs w-5 h-5 text-center mt-1">
+              {activatedCount}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`h-10 overflow-hidden transition-all duration-300 ease-in-out !flex justify-center items-center flex-row gap-2 w-fit ${expanded ? "animate-show" : "animate-hide-width"}  `}
+      >
+        {filter_options.map((option, index) => (
+          <div
+            onClick={() => {
+              const new_options = [...filter_options];
+              new_options[index].activated = !new_options[index].activated;
+              set_filter_options(new_options);
+            }}
+            key={index}
+            className={`w-8 h-8 bg-blue-500 ${option.activated ? "brightness-100" : "brightness-50"}  rounded-full p-1 flex items-center justify-center transition-all duration-100 hover:cursor-pointer hover:border-2 hover:border-gray-300`}
+          >
+            <Image src={option.icon} alt="icono" className="w-6 h-6" />
+          </div>
+        ))}
+        <div onClick={() => {
+          const new_options = [...filter_options];
+          new_options.forEach((option) => {
+            option.activated = false;
+          });
+          set_filter_options(new_options);
+          set_expanded(false);
+        }} className="w-7 h-7 bg-red-500 rounded-full p-1 flex items-center justify-center hover:cursor-pointer hover:border-2 hover:border-gray-300">
+        <IoClose />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/geographic-view/components/map-button.tsx
+++ src/features/geographic-view/components/map-button.tsx
@@ -2,55 +2,86 @@
 import { Button } from "flowbite-react";
 import { useState } from "react";
 import { IconType } from "react-icons";
+import Image from "next/image";
 
 type MapButtonType = {
   main_color: string;
   button_color: string;
-  icon: IconType;
+  icon: IconType | string;
+  icon_size?: string;
   text: string;
   open_to_left?: boolean;
   onClick?: () => void;
   activated?: boolean;
+  hover_disabled?: boolean;
 };
 
 export default function MapButton({
   main_color,
   button_color,
   icon: Icon,
+  icon_size = "w-5 h-5",
   text,
   open_to_left = false,
   onClick,
   activated = false,
+  hover_disabled = false,
 }: MapButtonType) {
   const [expanded, set_expanded] = useState(false);
 
   return (
-    <div className="w-fit flex flex-col items-end">
-      <div
-        className={`flex ${open_to_left ? "flex-row-reverse justify-self-end" : "flex-row"} ${main_color} transition-all duration-300 rounded-full ${expanded ? "w-full" : "w-10"}`}
-      >
-        <Button
-          onClick={onClick}
-          onMouseEnter={() => {
-            set_expanded(true);
-          }}
-          onMouseLeave={() => {
+    <div
+      className={`flex h-10 ${open_to_left ? "flex-row-reverse justify-self-end" : "flex-row"} ${main_color} transition-all duration-300 rounded-full w-fit ${expanded ? "max-w-full" : "max-w-10"}`}
+    >
+      <Button
+        onClick={() => {
+          if (!hover_disabled) {
             set_expanded(false);
-          }}
-          color="gray"
-          className={`flex align-middle justify-center h-10 w-10 transition-transform duration-100 z-20 ${button_color} rounded-full`}
-        >
+          }
+          onClick?.();
+        }}
+        onMouseEnter={() => {
+          if (!hover_disabled) {
+            set_expanded(true);
+          }
+        }}
+        onMouseLeave={() => {
+          set_expanded(false);
+        }}
+        color="gray"
+        className={`flex align-middle justify-center h-10 w-10 transition-transform duration-100 z-20 ${button_color} rounded-full`}
+        theme={{
+          base: "group relative flex items-stretch justify-center p-0 text-center font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none",
+          color:
+          {
+            gray: ":ring-cyan-700 border-gray-200 bg-white text-gray-900 focus:text-cyan-700 focus:ring-4 enabled:hover:bg-gray-100 enabled:hover:text-cyan-700 dark:border-gray-600 dark:bg-transparent dark:text-gray-400 dark:enabled:hover:bg-gray-700 dark:enabled:hover:text-white",
+          },
+          size: {
+            md: "px-0 py-0 w-10 h-10 flex items-center justify-center",
+          },
+        }}
+      >
+        {typeof Icon === "string" ? (
+          <div className={`${icon_size} flex items-center justify-center`}>
+            <Image
+              src={Icon}
+              alt="icono"
+              width={100}
+              height={100}
+            />
+          </div>
+        ) : (
           <Icon
-            className={`w-5 h-5 transition-transform duration-600 ${activated ? "rotate-180" : "rotate-0"} `}
+            className={`${icon_size} transition-transform duration-600 ${activated ? "rotate-180" : "rotate-0"} `}
           />
-        </Button>
-        <p
-          style={{ pointerEvents: "none" }}
-          className={` text-gray-800 dark:text-white text-[14px] font-size mx-5 h-10 flex items-center transition-all duration-300 whitespace-nowrap overflow-hidden ${expanded ? "opacity-100" : "opacity-0 overflow-hidden"}`}
-        >
-          {text}
-        </p>
-      </div>
+        )}
+      </Button>
+      <p
+        style={{ pointerEvents: "none" }}
+        className={`text-gray-800 dark:text-white text-[14px] font-size mx-5 h-10 flex items-center transition-all duration-300 whitespace-nowrap overflow-hidden ${expanded ? "opacity-100" : "opacity-0 overflow-hidden"}`}
+      >
+        {text}
+      </p>
     </div>
   );
 }

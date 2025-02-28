8675e1ca4b269013418e11a57e05181c67f12d77
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-02-28 17:17:52 -0300

    Temporarily comment out closing div in timed symptoms component

--- src/features/symptoms/components/symptoms-list/timed-symptoms.tsx
+++ src/features/symptoms/components/symptoms-list/timed-symptoms.tsx
@@ -97,7 +97,8 @@ export default function TimedSymptoms({
               </Button>
             </div>
           </div>
-        ))}
+        </div>
+        {/* ))} */}
       </div>
     </div>
   );

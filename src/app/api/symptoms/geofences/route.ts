e919969fc329ebb6dbbc2f604e0d5f34e561042d
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-03-03 20:24:29 -0300

    Refactor trip API route to use streaming for CSV data processing

---
+++ src/app/api/symptoms/geofences/route.ts
@@ -0,0 +1,57 @@
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+const SYMPTOMS_API_URL =
+  "https://pgrest.streamhub.cl:443/api/v1/pgrest/rpc/api_modular_treatments_geofences_service";
+
+import {
+  AuthToken,
+  AuthTokenConfig,
+} from "@/features/common/providers/sreamhub-api/streamhub-api.provider";
+
+const config: AuthTokenConfig = {
+  clientId: `${process.env.STREAMHUB_CLIENT_ID}`,
+  clientSecret: `${process.env.STREAMHUB_CLIENT_SECRET}`,
+  audience: `${process.env.STREAMHUB_AUDIENCE}`,
+  grantType: "client_credentials",
+};
+
+const authToken = new AuthToken(config);
+
+export async function GET() {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.next({
+      status: 401,
+    });
+  }
+
+  try {
+    const token = await authToken.getToken();
+    console.log(token);
+
+    const response = await fetch(SYMPTOMS_API_URL + "?p_trip_id=1446256", {
+      headers: {
+        accept: "application/json",
+        Authorization: ` Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const apiData = await response.json();
+
+    return NextResponse.json(apiData);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        data: {},
+        status: 500,
+        message: "Failed to fetch symptoms data",
+      },
+      { status: 500 },
+    );
+  }
+}

2a90ae562dcc6f450122a62633c6814fb4c383b6
khalzz <fearushbclear>
2025-03-13 17:48:23 -0300

    finished the tooltip

--- src/features/symptoms/components/table-item.tsx
+++ src/features/symptoms/components/table-item.tsx
@@ -23,7 +23,11 @@ export default function TableItem({
       >
         <TableCell className=" whitespace-nowrap font-medium text-gray-900 dark:text-white">
           <div className="flex items-center gap-2">
-            <ConditionIcon condition={data.condition} size="h-7 w-7" dict={dict} />
+            <ConditionIcon
+              condition={data.condition}
+              size="h-7 w-7"
+              dict={dict}
+            />
             <p
               className={`text-xs text-nowrap ${Conditions[data.condition as keyof typeof Conditions]?.textColor}`}
             >
@@ -111,7 +115,11 @@ export default function TableItem({
     >
       <TableCell className=" whitespace-nowrap font-medium text-gray-900 dark:text-white">
         <div className="flex items-center gap-2">
-          <ConditionIcon condition={data.condition} size="h-7 w-7" dict={dict} />
+          <ConditionIcon
+            condition={data.condition}
+            size="h-7 w-7"
+            dict={dict}
+          />
           <p
             className={`text-xs text-nowrap ${Conditions[data.condition as keyof typeof Conditions]?.textColor}`}
           >

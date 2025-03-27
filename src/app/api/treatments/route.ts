9041a29c26c122d2a1b63fb4256b632e3c50866b
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-03-27 12:26:31 -0300

    Refactor API URLs in route files to use environment variable for STREAMHUB_URL, improving configuration flexibility.

--- src/app/api/treatments/route.ts
+++ src/app/api/treatments/route.ts
@@ -1,8 +1,7 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
-const SYMPTOMS_API_URL =
-  "https://pgrest.streamhub.cl:443/api/v1/pgrest/rpc/process_treatment_manual_notifi_audit";
+const SYMPTOMS_API_URL = `${process.env.STREAMHUB_URL}/api/v1/pgrest/rpc/process_treatment_manual_notifi_audit`;
 
 import {
   AuthToken,

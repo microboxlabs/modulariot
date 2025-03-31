a10fad56aa2022bc598976cd6f5f71536a02493c
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-03-31 19:10:25 -0300

    Enhance Treatments API response and update SideInfoData component
    - Added 'assigned_to' and 'icu_code' fields to the TreatmentsTimelineResponse type.
    - Updated SideInfoData component to include 'assigned_to' in the data processing and display logic.
    - Improved UI to conditionally render 'assigned_to' information alongside treatment time details.

--- src/features/symptoms/components/map-view/side-info-data.tsx
+++ src/features/symptoms/components/map-view/side-info-data.tsx
@@ -118,6 +118,7 @@ export default function SideInfoData({
               icu_condition: event.icu_condition.toLowerCase(),
               description: event.description,
               type: event.type.toUpperCase(),
+              assigned_to: event.assigned_to,
             });
 
             return acc;
@@ -330,12 +331,25 @@ export default function SideInfoData({
                       </div>
                     </div>
                   </div>
-                  <div className="flex flex-col gap-3 bg">
+                  <div className="flex flex-col gap-3 ">
                     {item.items.reverse().map((subItem: any, subIndex: any) => (
                       <div
                         key={subIndex}
-                        className="flex flex-row gap-2"
+                        className={`flex flex-row gap-2 ${
+                          subItem.icu_condition == "-1" ||
+                          subItem.type == "EVENTS END" ||
+                          subItem.type == "TRIP_START"
+                            ? "bg-gray-200 rounded-md"
+                            : ""
+                        }`}
                         onClick={(e) => {
+                          if (
+                            subItem.icu_condition == "-1" ||
+                            subItem.type == "EVENTS END" ||
+                            subItem.type == "TRIP_START"
+                          ) {
+                            return;
+                          }
                           e.stopPropagation();
                           setSelectedTreatment(treatmentData);
                           setSelectedTreatmentIndex(subItem);
@@ -349,18 +363,30 @@ export default function SideInfoData({
                           />
                           <div className="w-[2px] mt-1 mx-auto bg-gray-400 flex-grow" />
                         </div>
-                        <div className="flex flex-col">
-                          <p className="h-7 text-sm font-medium text-gray-600 dark:text-gray-300 flex items-center">
-                            {new Date(subItem.start).toLocaleTimeString([], {
-                              hour: "2-digit",
-                              minute: "2-digit",
-                            })}{" "}
-                            |{" "}
-                            {new Date(subItem.end).toLocaleTimeString([], {
-                              hour: "2-digit",
-                              minute: "2-digit",
-                            })}
-                          </p>
+                        <div className="w-full flex flex-col">
+                          <div className="flex flex-row gap-2">
+                            <p className="h-7 text-sm font-medium text-gray-600 dark:text-gray-300 flex items-center">
+                              {new Date(subItem.start).toLocaleTimeString([], {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              })}{" "}
+                              |{" "}
+                              {new Date(subItem.end).toLocaleTimeString([], {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              })}{" "}
+                            </p>
+                            {subItem.assigned_to && (
+                              <p className="flex flex-row flex-grow justify-end">
+                                <small className="bg-blue-200 rounded-md px-2  py-1 text-gray-600 flex items-center text-xs">
+                                  {((dict.symptoms as I18nRecord)[
+                                    subItem.assigned_to
+                                  ] as string) ?? subItem.assigned_to}
+                                </small>
+                              </p>
+                            )}
+                          </div>
+
                           <div className="flex flex-col">
                             <p className="text-sm font-medium text-gray-900 dark:text-gray-200">
                               {((dict.symptoms as I18nRecord)[

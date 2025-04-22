df01fcfdc3723227448c2a36a31e22e52885cabc
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-04-22 09:57:36 -0400

    Add mintral_hoReference to TaskResponse type and enhance DownloadSignedDocument component
    - Introduced mintral_hoReference property in TaskResponse type for better task data handling.
    - Updated DownloadSignedDocument component to support rendering as a link with customizable name.
    - Integrated DownloadSignedDocument into TripInformation component to display document link when mintral_hoReference is available.

--- src/features/shipping/components/download-signed-document/download-signed-document.tsx
+++ src/features/shipping/components/download-signed-document/download-signed-document.tsx
@@ -1,14 +1,18 @@
-import { Download } from "flowbite-react-icons/outline";
+import { ArrowRight, Download } from "flowbite-react-icons/outline";
 import { Button } from "flowbite-react";
 import Link from "next/link";
 import { useVerifyDocument } from "@/features/common/providers/client-api.provider";
 
 interface DownloadSignedDocumentProps {
   documentId?: string;
+  asLink?: boolean;
+  name?: string;
 }
 
 export default function DownloadSignedDocument({
   documentId,
+  asLink = false,
+  name = "carta porte",
 }: DownloadSignedDocumentProps) {
   const documentPath = documentId?.replace(":/", "");
   const href = `/api/document/download?documentId=${documentPath}`;
@@ -16,23 +20,31 @@ export default function DownloadSignedDocument({
   const { exists } = useVerifyDocument(uuid?.[uuid.length - 1] || "");
 
   return (
-    <div className="flex items-start rounded-lg text-sm font-medium h-7">
-      {exists ? (
-        <Button
-          className="group"
-          outline
-          as={Link}
-          color="blue"
-          size="xs"
-          href={href}
-        >
-          <Download className="h-4 w-4 text-blue-700" />
-        </Button>
+    <>
+      {asLink ? (
+        <Link className="text-blue-700 text-xs cursor-pointer flex" href={href}>
+          {`${name}`} <ArrowRight className="h-4 w-4 text-blue-700" />
+        </Link>
       ) : (
-        <Button className="group" outline color="blue" size="xs" disabled>
-          <Download className="h-4 w-4 text-blue-700" />
-        </Button>
+        <div className="flex items-start rounded-lg text-sm font-medium h-7">
+          {exists ? (
+            <Button
+              className="group"
+              outline
+              as={Link}
+              color="blue"
+              size="xs"
+              href={href}
+            >
+              <Download className="h-4 w-4 text-blue-700" />
+            </Button>
+          ) : (
+            <Button className="group" outline color="blue" size="xs" disabled>
+              <Download className="h-4 w-4 text-blue-700" />
+            </Button>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 }

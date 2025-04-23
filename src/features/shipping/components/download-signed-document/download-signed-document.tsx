451af0e38b21f4b14c1368223bdd4d7b76d4ce21
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-04-23 13:50:22 -0400

    Comment out document verification logic in DownloadSignedDocument component for future implementation

--- src/features/shipping/components/download-signed-document/download-signed-document.tsx
+++ src/features/shipping/components/download-signed-document/download-signed-document.tsx
@@ -1,7 +1,7 @@
 import { Download } from "flowbite-react-icons/outline";
 import { Button } from "flowbite-react";
 import Link from "next/link";
-import { useVerifyDocument } from "@/features/common/providers/client-api.provider";
+//import { useVerifyDocument } from "@/features/common/providers/client-api.provider";
 
 interface DownloadSignedDocumentProps {
   documentId?: string;
@@ -16,8 +16,8 @@ export default function DownloadSignedDocument({
 }: DownloadSignedDocumentProps) {
   const documentPath = documentId?.replace(":/", "");
   const href = `/api/document/download?documentId=${documentPath}`;
-  const uuid = documentId?.split("/");
-  const { exists } = useVerifyDocument(uuid?.[uuid.length - 1] || "");
+  //const uuid = documentId?.split("/");
+  /* const { exists } = useVerifyDocument(uuid?.[uuid.length - 1] || ""); */
 
   return (
     <>
@@ -39,22 +39,22 @@ export default function DownloadSignedDocument({
         </div>
       ) : (
         <div className="flex items-start rounded-lg text-sm font-medium h-7">
-          {exists ? (
-            <Button
-              className="group"
-              outline
-              as={Link}
-              color="blue"
-              size="xs"
-              href={href}
-            >
-              <Download className="h-4 w-4 text-blue-700" />
-            </Button>
-          ) : (
+          {/* {exists ? ( */}
+          <Button
+            className="group"
+            outline
+            as={Link}
+            color="blue"
+            size="xs"
+            href={href}
+          >
+            <Download className="h-4 w-4 text-blue-700" />
+          </Button>
+          {/*  ) : (
             <Button className="group" outline color="blue" size="xs" disabled>
               <Download className="h-4 w-4 text-blue-700" />
             </Button>
-          )}
+          )} */}
         </div>
       )}
     </>

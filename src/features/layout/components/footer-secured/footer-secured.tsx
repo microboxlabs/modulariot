b8b306f761442b97a2e75ae4258064429a75e945
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-04-24 12:45:19 -0400

    Refactor FooterSecuredLayout component for improved styling
    - Updated footer class from "flex-row" to "flex" for better layout handling.
    - Enhanced theme base styling with additional padding and adjusted alignment for improved visual consistency.

--- src/features/layout/components/footer-secured/footer-secured.tsx
+++ src/features/layout/components/footer-secured/footer-secured.tsx
@@ -6,10 +6,10 @@ export default function FooterSecuredLayout({ messages }: FooterSecuredProps) {
   return (
     <Footer
       date-testid="secured-footer"
-      className="flex-row z-40 h-12 lg:px-5"
+      className="flex z-40 h-12 lg:px-5"
       theme={{
         root: {
-          base: "w-full bg-white fixed bottom-0 shadow dark:bg-gray-800 md:flex md:items-center md:justify-between",
+          base: "w-full bg-white fixed bottom-0 shadow dark:bg-gray-800 md:flex md:items-center justify-between px-5 py-3",
         },
       }}
     >

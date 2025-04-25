59994d11d029024b894ab08061330e43f0690747
khalzz <fearushbclear>
2025-04-25 16:04:00 -0400

    build fix

--- src/features/symptoms/table.tsx
+++ src/features/symptoms/table.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import React, { useRef, useState } from "react";
-import { Button, TextInput, Label, Dropdown } from "flowbite-react";
-import { HiSearch } from "react-icons/hi";
+import React, { useState } from "react";
+import { Button } from "flowbite-react";
 import { FiMaximize, FiMinimize } from "react-icons/fi";
-import { FaFilter, FaArrowsRotate } from "react-icons/fa6";
+import { FaArrowsRotate } from "react-icons/fa6";
 import TableComponent from "./components/table-component";
 import { I18nRecord } from "@/features/i18n/i18n.service.types";
-import icu_code from "./model/icu_condition.json";
 import UserStateCounter from "./components/user-counter";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";

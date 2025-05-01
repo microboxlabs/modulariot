f567ca87a946e03f705a1b9405dd30f3d0822c7c
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-05-01 16:57:40 -0400

    Add SVG icons for various timeline states and symptoms
    - Introduced new SVG files for continuous drive check, continuous resting check, lost signal, off-hour driving, overnight risk stay, speed limit standard, and speed limit custom.
    - Updated symptom icon component to utilize the new SVGs.
    - Enhanced the table item type definitions to include new symptom icons and their properties.
    - Modified the timeline component to incorporate the new icons for improved visual representation of symptoms and conditions.

--- src/features/symptoms/components/table-item.type.ts
+++ src/features/symptoms/components/table-item.type.ts
@@ -6,6 +6,11 @@ import observation from "@assets/conditions/en-observacion.svg";
 import treatment from "@assets/conditions/en-tratamiento.svg";
 import stable from "@assets/conditions/estable.svg";
 
+import continuousRestingCheck from "@assets/timeline/continuous-resting-check.svg";
+import lostSignal from "@assets/timeline/lost-signal.svg";
+import speedLimitStandard from "@assets/timeline/speed-limit-standar.svg";
+import speedLimitCustom from "@assets/timeline/speed-limit-custom.svg";
+import maximumContinuousDriving from "@assets/timeline/continuos-drive-check.svg";
 export type TableItemType = {
   id: string;
   condition: string;
@@ -98,6 +103,164 @@ export const Conditions: Record<string, Condition> = {
     separatorColor: "border-gray-300 dark:border-gray-700",
     hoverColor: "",
     icon: treatment,
+  },
+  stable: {
+    dict_name: "stable",
+    color: "border-blue-600",
+    innerColor: "bg-blue-100",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: stable,
+  },
+  compromised: {
+    dict_name: "compromised_condition",
+    color: "border-rose-700",
+    innerColor: "bg-rose-50",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: compromised,
+  },
+  "compromised condition": {
+    dict_name: "compromised_condition",
+    color: "border-rose-700",
+    innerColor: "bg-rose-50",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: compromised,
+  },
+  "under observation": {
+    dict_name: "in_observation",
+    color: "border-rose-700",
+    textColor: "text-black dark:text-white",
+    innerColor: "bg-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: observation,
+  },
+  observation: {
+    dict_name: "in_observation",
+    color: "border-rose-700",
+    textColor: "text-black dark:text-white",
+    innerColor: "bg-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: observation,
+  },
+  remission: {
+    dict_name: "in_remission",
+    color: "border-teal-700",
+    innerColor: "bg-teal-50",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: recovery,
+  },
+  "ignore condition": {
+    dict_name: "in_remission",
+    color: "border-teal-700",
+    innerColor: "bg-teal-50",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: recovery,
+  },
+};
+
+export const Symptoms: Record<string, any> = {
+  "CONTINUOUS RESTING CHECK": {
+    dict_name: "code_black",
+    color: "border-black",
+    bgColor: "!bg-black",
+    hoverColor: "hover:!bg-gray-900 dark:hover:!bg-gray-900",
+    innerColor: "bg-gray-200",
+    textColor: "text-white",
+    secundaryInteraction: "border-gray-700 bg-gray-800 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-200",
+    separatorColor: "border-gray-700",
+    icon: continuousRestingCheck,
+    borderColor: "border-gray-200 dark:border-gray-200",
+  },
+  "LOST SIGNAL": {
+    dict_name: "critical_condition",
+    color: "border-red-500",
+    bgColor: "!bg-rose-500",
+    hoverColor: "hover:!bg-rose-400 dark:hover:!bg-rose-400",
+    innerColor: "bg-rose-200",
+    textColor: "text-white",
+    secundaryInteraction: "border-gray-700 bg-gray-800 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-200",
+    icon: lostSignal,
+    borderColor: "border-gray-200 dark:border-gray-200",
+  },
+  "SPEED LIMIT STANDARD": {
+    dict_name: "critical_condition",
+    color: "border-red-500",
+    bgColor: "!bg-rose-500",
+    hoverColor: "hover:!bg-rose-400 dark:hover:!bg-rose-400",
+    innerColor: "bg-rose-200",
+    textColor: "text-white",
+    secundaryInteraction: "border-gray-300 bg-gray-100 dark:bg-gray-100",
+    secundaryInteractionIcon: "text-gray-700",
+    separatorColor: "border-gray-100",
+    icon: speedLimitStandard,
+    borderColor: "border-gray-200 dark:border-gray-200",
+  },
+  "SPEED LIMIT CUSTOM": {
+    dict_name: "in_treatment",
+    color: "border-amber-500",
+    innerColor: "bg-amber-100",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: speedLimitCustom,
+  },
+  "CONTINUOUS DRIVE CHECK": {
+    dict_name: "in_treatment",
+    color: "border-amber-500",
+    innerColor: "bg-amber-100",
+    textColor: "text-black dark:text-white",
+    bgColor: "border border-gray-300 dark:border-gray-700",
+    secundaryInteraction:
+      "border-gray-400 dark:border-gray-600 bg-gray-100 dark:bg-gray-800",
+    secundaryInteractionIcon: "text-gray-700 dark:text-gray-300",
+    separatorColor: "border-gray-300 dark:border-gray-700",
+    hoverColor: "",
+    icon: maximumContinuousDriving,
   },
   stable: {
     dict_name: "stable",

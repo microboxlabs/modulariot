2d75c210982380aebf84a81397abb296d175a39a
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-06-02 17:36:25 -0400

    Implement user group permissions in SideInfo and TaskActions components
    - Integrated `GroupAllowed` component to manage user group permissions in both `SideInfo` and `TaskActions`.
    - Utilized `useSession` from `next-auth` to retrieve user groups and conditionally render UI elements based on permissions.
    - Enhanced user experience by restricting access to certain actions for users in the "GROUP_MINTRAL_REVISOR" group.

--- src/features/symptoms/side-info.tsx
+++ src/features/symptoms/side-info.tsx
@@ -13,6 +13,9 @@ import TimelineComponent from "./components/map-view/timeline";
 import { FaClock } from "react-icons/fa";
 import { HiMiniArrowPathRoundedSquare } from "react-icons/hi2";
 import { ConditionsAgg } from "./types/timeline";
+import { GroupAllowed } from "../common/components/group-allowed/group-allowed";
+import { useSession } from "next-auth/react";
+//import { getUserGroupsLabels } from "../auth/utils/utils";
 export default function SideInfo({
   dict,
   treatmentData,
@@ -37,6 +40,10 @@ export default function SideInfo({
     treatmentData?.symptom_info?.name ?? "Bad Sign",
     treatmentData?.symptom_info?.icu_code.toString() ?? "4",
   );
+
+  const { data: session } = useSession();
+
+  const userGroups = session?.user.groups ?? []; //(getUserGroupsLabels(session?.user.groups as string[]) ??[]) as string[];
 
   /*
   if (loading) {
@@ -116,26 +123,31 @@ export default function SideInfo({
       <div
         className={`absolute bottom-5 left-5 right-5 flex flex-col justify-self-end w-full px-5 ${loading ? "opacity-50" : "opacity-100"}`}
       >
-        <Button.Group className="w-full">
-          <BlurrableDropdown
-            dict={dict}
-            isMenuOpen={isMenuOpen}
-            setIsMenuOpen={setIsMenuOpen}
-            setSelectedOption={setSelectedOption}
-          />
-          <Button
-            size="md"
-            color="blue"
-            className="h-10 rounded-l-none w-full whitespace-nowrap"
-            onClick={() => {
-              setIsMenuOpen(!isMenuOpen);
-              setSelectedOption("call_driver");
-            }}
-          >
-            {(dict.symptoms as I18nRecord).call_driver as string}
-            <HiArrowRight className="ml-2 h-5 w-5 xl:flex lg:hidden" />
-          </Button>
-        </Button.Group>
+        <GroupAllowed
+          notAllowedTo={["GROUP_MINTRAL_REVISOR"]}
+          userGroups={userGroups}
+        >
+          <Button.Group className="w-full">
+            <BlurrableDropdown
+              dict={dict}
+              isMenuOpen={isMenuOpen}
+              setIsMenuOpen={setIsMenuOpen}
+              setSelectedOption={setSelectedOption}
+            />
+            <Button
+              size="md"
+              color="blue"
+              className="h-10 rounded-l-none w-full whitespace-nowrap"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+                setSelectedOption("call_driver");
+              }}
+            >
+              {(dict.symptoms as I18nRecord).call_driver as string}
+              <HiArrowRight className="ml-2 h-5 w-5 xl:flex lg:hidden" />
+            </Button>
+          </Button.Group>
+        </GroupAllowed>
       </div>
     </div>
   );

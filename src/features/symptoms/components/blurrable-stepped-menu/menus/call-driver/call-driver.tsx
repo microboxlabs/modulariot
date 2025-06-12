7db9f56a9e9b0094d3632ffad7bfbf077b245ed3
khalzz <fearushbclear>
2025-06-12 17:42:40 -0400

    asredesign of mapview forms

--- src/features/symptoms/components/blurrable-stepped-menu/menus/call-driver/call-driver.tsx
+++ src/features/symptoms/components/blurrable-stepped-menu/menus/call-driver/call-driver.tsx
@@ -1,7 +1,25 @@
-import { Textarea } from "flowbite-react";
+"use client";
+
+import { Button, Textarea } from "flowbite-react";
 import { I18nRecord } from "@/features/i18n/i18n.service.types";
 import { TreatmentsGeneralResponseItem } from "@/app/api/treatments/general/route.type";
 import { useState } from "react";
+import TagManager from "@/features/symptoms/components/tag-manager";
+import { requestTreatment } from "@/features/common/providers/client-api.provider";
+import { TreatmentsRequest } from "@/app/api/treatments/route.type";
+import { BiLogoMicrosoftTeams } from "react-icons/bi";
+import { useRouter } from "next/navigation";
+
+const sendTeamsCall = async (phoneNumber: string) => {
+  // Get the phone number from the treatment request
+  if (!phoneNumber) return;
+
+  // Open Teams with the phone number parameter
+  window.open(
+    `https://teams.microsoft.com/l/call/0/0?users=4:${phoneNumber}`,
+    "_blank",
+  );
+};
 
 export default function CallDriver({
   dict,
@@ -10,6 +28,9 @@ export default function CallDriver({
   setMessageToCommunicate,
   driverResponse,
   setDriverResponse,
+  treatmentRequest,
+  setTreatmentRequest,
+  setIsMenuOpen,
 }: {
   dict: I18nRecord;
   treatmentData: TreatmentsGeneralResponseItem | null;
@@ -17,10 +38,64 @@ export default function CallDriver({
   setMessageToCommunicate: (message: string) => void;
   driverResponse: string;
   setDriverResponse: (driverResponse: string) => void;
+  treatmentRequest: TreatmentsRequest;
+  setTreatmentRequest: (treatmentRequest: TreatmentsRequest) => void;
+  setIsMenuOpen: (isMenuOpen: boolean) => void;
 }) {
+  const [selectedTags, setSelectedTags] = useState<Array<{ text: string }>>([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const router = useRouter();
+
+  const handleTagCreate = () => {
+    if (inputValue.trim()) {
+      setSelectedTags([...selectedTags, { text: inputValue.trim() }]);
+      setInputValue("");
+    }
+  };
+
+  const handleTagRemove = (index: number) => {
+    const newTags = selectedTags.filter((_, i) => i !== index);
+    setSelectedTags(newTags);
+  };
+
+  const predefinedTags = [
+    { text: "Ruta con problemas" },
+    { text: "Conductor problemático" },
+    { text: "Prueba" },
+  ];
+
+  const buttons = [
+    {
+      color: "light",
+      text: (dict.symptoms as I18nRecord).teams_call,
+      text2: (dict.symptoms as I18nRecord).teams_call2,
+      icon: <BiLogoMicrosoftTeams className="w-6 h-6 ml-2 mr-2" />,
+      function: async () => {
+        await sendTeamsCall(treatmentData?.trip_info?.driver_contact ?? "");
+      },
+    },
+    {
+      color: "blue",
+      text: (dict.symptoms as I18nRecord).save_treatment,
+      function: async () => {
+        const response = await requestTreatment(treatmentRequest);
+        console.log(treatmentRequest);
+        console.log(response.treatment_id);
+
+        setTreatmentRequest({
+          ...treatmentRequest,
+          treatment_id: response.treatment_id,
+        });
+        setIsMenuOpen(false);
+        router.push("/symptoms");
+      },
+    },
+  ];
+
   return (
     <div className="h-full w-full flex flex-col items-center justify-center gap-2">
-      <div className=" w-full flex flex-col items-center gap-2 flex-grow">
+      <div className=" w-full flex flex-col items-center flex-grow">
         <div className="w-full flex flex-col gap-2">
           <h1 className="w-full text-left text-sm font-light justify-self-end text-gray-900 dark:text-white">
             {(dict.symptoms as I18nRecord).service_information as string}
@@ -72,7 +147,7 @@ export default function CallDriver({
             </div>
           </div>
         </div>
-        <hr className="w-full border-gray-200 dark:border-gray-700" />
+        <hr className="w-full border-gray-200 dark:border-gray-700 mt-2 mb-3" />
         <div className="w-full flex flex-row gap-5">
           <div className="w-1/2 flex flex-col gap-2">
             <h1 className="w-full text-left text-sm font-light justify-self-end text-gray-900 dark:text-white">
@@ -98,6 +173,77 @@ export default function CallDriver({
               onChange={(e) => setDriverResponse(e.target.value)}
             />
           </div>
+        </div>
+        <hr className="w-full border-gray-200 dark:border-gray-700 mt-2 mb-3" />
+        <div className="w-full flex flex-col">
+          <h1 className="w-full text-left text-sm font-light justify-self-end text-gray-900 dark:text-white">
+            Tags
+          </h1>
+          <div className="flex flex-wrap gap-2 mb-2">
+            {selectedTags.length > 0 && (
+              <TagManager
+                tags={selectedTags.map((tag, index) => ({
+                  text: tag.text,
+                  icon: (
+                    <button
+                      onClick={() => handleTagRemove(index)}
+                      className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                    >
+                      ×
+                    </button>
+                  ),
+                }))}
+                tag_style="bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white"
+              />
+            )}
+          </div>
+          <div className="flex gap-2 flex-wrap">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  handleTagCreate();
+                }
+              }}
+              placeholder="Ingresa un tag"
+              className="flex-1 px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-600 focus:border-blue-500 dark:focus:border-blue-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
+            />
+          </div>
+
+          <div className="flex flex-wrap gap-2 mt-2">
+            {predefinedTags.map((tag, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  if (!selectedTags.some((t) => t.text === tag.text)) {
+                    setSelectedTags([...selectedTags, tag]);
+                  }
+                }}
+                className="px-3 py-1 text-sm border border-gray-300 dark:border-gray-600 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 text-gray-900 dark:text-white"
+              >
+                {tag.text}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="w-full flex flex-row gap-2 overflow-visible mt-4">
+          {buttons.map((button, index) => (
+            <Button
+              className="flex-1"
+              key={index}
+              color={button.color}
+              onClick={() => {
+                button.function();
+              }}
+            >
+              {button.text as string}
+              {button.icon}
+              {button.text2 as string}
+            </Button>
+          ))}
         </div>
       </div>
     </div>

42c5bdfea6621a781de8d01b5aa226a0c6edbdc0
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-06-12 12:56:37 -0400

    Refactor SignInPage component layout for improved structure
    - Removed SessionCheck wrapper and adjusted layout to enhance readability.
    - Maintained existing functionality while ensuring consistent UI with NavbarSignIn and FooterSignIn components.
    
    (cherry picked from commit e4c4b5a40be2169f90666b06099e8ab4f1b04367)

--- src/app/[lang]/sign-in/page.tsx
+++ src/app/[lang]/sign-in/page.tsx
@@ -6,39 +6,36 @@ import FooterSignIn from "@/features/auth/components/footer-sign-in/footer-sign-
 import FormSignIn from "@/features/auth/components/form-sign-in/form-sign-in";
 import { buildSignInFormMessages } from "@/features/auth/utils/utils";
 import { ParamsWithLang } from "@/features/i18n/i18n.service.types";
-import { SessionCheck } from "@/app/[lang]/sign-in/session-check";
 
 export default async function SignInPage({ params: { lang } }: ParamsWithLang) {
   const [dict] = await getDictionary(lang);
   const signInMessages = buildSignInFormMessages({ messages: dict });
 
   return (
-    <SessionCheck lang={lang}>
-      <div className="mx-auto flex flex-col px-6 pt-8 md:h-screen">
-        <NavbarSignIn />
-        <div className="flex flex-1 flex-col items-center justify-center">
-          <div className="w-full md:max-w-lg">
-            <Card
-              data-testid="login-card"
-              horizontal
-              imgAlt=""
-              className="bg-white dark:bg-gray-800 transition-colors duration-200"
-              theme={{
-                root: {
-                  base: "flex rounded-lg border border-gray-200 shadow-md dark:border-gray-700",
-                  children: "my-auto w-full gap-0 space-y-8 p-6 sm:p-4 lg:p-8",
-                },
-              }}
-            >
-              <h2 className="text-2xl font-bold text-gray-900 lg:text-3xl dark:text-white">
-                {dict("pages.login.welcome")}
-              </h2>
-              <FormSignIn messages={signInMessages} />
-            </Card>
-          </div>
+    <div className="mx-auto flex flex-col px-6 pt-8 md:h-screen">
+      <NavbarSignIn />
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <div className="w-full md:max-w-lg">
+          <Card
+            data-testid="login-card"
+            horizontal
+            imgAlt=""
+            className="bg-white dark:bg-gray-800 transition-colors duration-200"
+            theme={{
+              root: {
+                base: "flex rounded-lg border border-gray-200 shadow-md dark:border-gray-700",
+                children: "my-auto w-full gap-0 space-y-8 p-6 sm:p-4 lg:p-8",
+              },
+            }}
+          >
+            <h2 className="text-2xl font-bold text-gray-900 lg:text-3xl dark:text-white">
+              {dict("pages.login.welcome")}
+            </h2>
+            <FormSignIn messages={signInMessages} />
+          </Card>
         </div>
-        <FooterSignIn messages={dict} />
       </div>
-    </SessionCheck>
+      <FooterSignIn messages={dict} />
+    </div>
   );
 }

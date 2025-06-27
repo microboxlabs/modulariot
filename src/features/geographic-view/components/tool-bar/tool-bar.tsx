c02621c5e9b2d103607a9544847eef06c6e60e6c
khalzz <fearushbclear>
2025-06-27 16:48:22 -0400

    Fixed notifications format and tooltip add in toolbar in map view

--- src/features/geographic-view/components/tool-bar/tool-bar.tsx
+++ src/features/geographic-view/components/tool-bar/tool-bar.tsx
@@ -9,6 +9,8 @@ import { mapstyles } from "../map-style-selector";
 import PulseRange from "./pulse-range";
 import { BsSignStop } from "react-icons/bs";
 import Screenshot from "./screenshot";
+import { Tooltip } from "flowbite-react";
+import { I18nRecord } from "@/features/i18n/i18n.service.types";
 
 type ToolBarProps = {
   positions: MapPosition[] | null;
@@ -35,6 +37,7 @@ type ToolBarProps = {
   setShowGeofences: (showGeofences: boolean) => void;
   showPulse: boolean;
   setShowPulse: (showPulse: boolean) => void;
+  dictionary: I18nRecord;
 };
 
 export default function ToolBar({
@@ -54,6 +57,7 @@ export default function ToolBar({
   setShowGeofences,
   showPulse,
   setShowPulse,
+  dictionary,
 }: ToolBarProps) {
   const [open, setOpen] = useState(false);
   const [selectedTool, setSelectedTool] = useState<
@@ -110,68 +114,156 @@ export default function ToolBar({
           className={`flex flex-row gap-3 text-sm text-gray-500 transition-all duration-300 overflow-hidden rounded-md ${!open ? "max-w-0 p-0" : "max-w-96 p-1"} ${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "bg-white" : "bg-gray-800"}`}
         >
           <div className="flex flex-row gap-1">
-            <div
-              className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-md hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${selectedTool === "mapSelection" ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
-              onClick={() => {
-                setSelectedTool(
-                  selectedTool === "mapSelection" ? null : "mapSelection",
-                );
-              }}
-            >
-              <FaMap size={20} />
-            </div>
-            <div
-              className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-md hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${selectedTool === "timeline" ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
-              onClick={() =>
-                setSelectedTool(selectedTool === "timeline" ? null : "timeline")
+            <Tooltip
+              content={
+                ((dictionary as I18nRecord).symptoms as I18nRecord)
+                  .mapSelection as string
+              }
+              style={
+                mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                  ? "light"
+                  : "dark"
               }
             >
-              <MdOutlineTimeline size={20} />
-            </div>
+              <div
+                className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-md hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${selectedTool === "mapSelection" ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
+                onClick={() => {
+                  setSelectedTool(
+                    selectedTool === "mapSelection" ? null : "mapSelection",
+                  );
+                }}
+              >
+                <FaMap size={20} />
+              </div>
+            </Tooltip>
+            <Tooltip
+              content={
+                ((dictionary as I18nRecord).symptoms as I18nRecord)
+                  .timeline as string
+              }
+              style={
+                mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                  ? "light"
+                  : "dark"
+              }
+            >
+              <div
+                className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-md hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${selectedTool === "timeline" ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
+                onClick={() =>
+                  setSelectedTool(
+                    selectedTool === "timeline" ? null : "timeline",
+                  )
+                }
+              >
+                <MdOutlineTimeline size={20} />
+              </div>
+            </Tooltip>
           </div>
           {/* Map toggles */}
           <div className="flex flex-row gap-1">
-            <div
-              className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-full hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${showPulse ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
-              onClick={() => setShowPulse(!showPulse)}
+            <Tooltip
+              content={
+                ((dictionary as I18nRecord).symptoms as I18nRecord)
+                  .pulse as string
+              }
+              style={
+                mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                  ? "light"
+                  : "dark"
+              }
             >
-              <div className="w-5 h-5 bg-blue-500 border-2 border-white rounded-full"></div>
-            </div>
-            <div
-              className={`flex justify-center items-center border-2 border-gray-400 aspect-square h-8 w-8 rounded-full hover:border-blue-500 cursor-pointer pointer-events-auto ${showStops ? "bg-blue-500 text-white" : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
-              onClick={() => setShowStops(!showStops)}
+              <div
+                className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-full hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${showPulse ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
+                onClick={() => setShowPulse(!showPulse)}
+              >
+                <div className="w-5 h-5 bg-blue-500 border-2 border-white rounded-full"></div>
+              </div>
+            </Tooltip>
+            <Tooltip
+              content={
+                ((dictionary as I18nRecord).symptoms as I18nRecord)
+                  .stops as string
+              }
+              style={
+                mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                  ? "light"
+                  : "dark"
+              }
             >
-              <BsSignStop size={20} />
-            </div>
-            <div
-              className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-full hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${showGeofences ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
-              onClick={() => setShowGeofences(!showGeofences)}
+              <div
+                className={`flex justify-center items-center border-2 border-gray-400 aspect-square h-8 w-8 rounded-full hover:border-blue-500 cursor-pointer pointer-events-auto ${showStops ? "bg-blue-500 text-white" : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
+                onClick={() => setShowStops(!showStops)}
+              >
+                <BsSignStop size={20} />
+              </div>
+            </Tooltip>
+            <Tooltip
+              content={
+                ((dictionary as I18nRecord).symptoms as I18nRecord)
+                  .geofences as string
+              }
+              style={
+                mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                  ? "light"
+                  : "dark"
+              }
             >
-              <FaMapPin size={20} />
-            </div>
+              <div
+                className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-full hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${showGeofences ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
+                onClick={() => setShowGeofences(!showGeofences)}
+              >
+                <FaMapPin size={20} />
+              </div>
+            </Tooltip>
           </div>
           {/* Map toggles */}
 
           {/* Action toggles */}
           <div className="flex flex-row gap-1">
-            <div
-              className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-md hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${camera_movement ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
-              onClick={() => {
-                setCameraMovement(!camera_movement);
-              }}
+            <Tooltip
+              content={
+                ((dictionary as I18nRecord).symptoms as I18nRecord)
+                  .cameraMovement as string
+              }
+              style={
+                mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                  ? "light"
+                  : "dark"
+              }
             >
-              {camera_movement ? (
-                <MdGpsFixed size={20} />
-              ) : (
-                <MdGpsNotFixed size={20} />
-              )}
-            </div>
+              <div
+                className={`border-2 border-gray-400 aspect-square h-8 w-8 rounded-md hover:border-blue-500 cursor-pointer pointer-events-auto flex items-center justify-center ${camera_movement ? "bg-blue-500 text-white " : `${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`} `}
+                onClick={() => {
+                  setCameraMovement(!camera_movement);
+                }}
+              >
+                {camera_movement ? (
+                  <MdGpsFixed size={20} />
+                ) : (
+                  <MdGpsNotFixed size={20} />
+                )}
+              </div>
+            </Tooltip>
           </div>
           {/* Action toggles */}
           {/* General Actions */}
-          <div className="flex flex-row gap-1">
-            <Screenshot />
-          </div>
+          <Tooltip
+            content={
+              ((dictionary as I18nRecord).symptoms as I18nRecord)
+                .screenshot as string
+            }
+            style={
+              mapstyles.find((style) => style.value === selectedStyle)?.isDark
+                ? "light"
+                : "dark"
+            }
+          >
+            <div
+              className={`flex flex-row gap-1 ${mapstyles.find((style) => style.value === selectedStyle)?.isDark ? "text-gray-500" : "text-gray-300"}`}
+            >
+              <Screenshot />
+            </div>
+          </Tooltip>
           {/* General Actions */}
         </div>
       </div>

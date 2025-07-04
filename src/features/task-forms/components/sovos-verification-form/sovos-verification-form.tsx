4ee185a7af0a5b78e6438dd80846a630f248d557
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-07-03 21:16:06 -0400

    Fix early return in SovosVerificationForm component to ensure proper flow

--- src/features/task-forms/components/sovos-verification-form/sovos-verification-form.tsx
+++ src/features/task-forms/components/sovos-verification-form/sovos-verification-form.tsx
@@ -81,7 +81,7 @@ export default function SovosVerificationForm({
             ...stepper,
             isError: true,
           });
-          //return;
+          return;
         }
       }
       //pos++;

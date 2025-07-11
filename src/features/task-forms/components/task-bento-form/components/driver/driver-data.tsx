e5ae9e6ce2e56080a5861e5ba0438b5bae4dff78
khalzz <fearushbclear>
2025-07-11 14:31:15 -0400

    advancing more on the bentobox

--- src/features/task-forms/components/task-bento-form/components/driver/driver-data.tsx
+++ src/features/task-forms/components/task-bento-form/components/driver/driver-data.tsx
@@ -5,73 +5,76 @@ import { IoIosMail } from "react-icons/io";
 import { Driver } from "../../../driver-contact-info/driver-contact-info.type";
 import DriverValidations from "./driver-validations";
 
-export default function DriverData(
-  {
-    driver,
-    msg,
-  }: {
-    driver: Driver;
-    msg: I18nRecord;
-  }
-) {
+export default function DriverData({
+  driver,
+  msg,
+}: {
+  driver: Driver;
+  msg: I18nRecord;
+}) {
   console.log(driver);
 
   return (
-    <div className="flex flex-col gap-1 items-center h-full">
-        <h1 className="text-md font-normal text-gray-700 dark:text-gray-300 flex flex-row gap-2 items-center">
-          {driver.name}
-          <Tooltip
-            style="auto"
-            content={
-              <div className="text-gray-600 dark:text-gray-400 text-sm flex gap-1 items-center w-fit font-light">
-                Estado: <span className="text-gray-800 dark:text-gray-100 text-sm">{driver.status}</span>
-              </div>
-            }
-          >
-            {
-              driver.status === "verified" && (
-                <div className="border border-green-500 bg-green-500 rounded-full p-1">
-                  <FaCheck className="w-4 h-4 text-white" />
-                </div>
-              )
-            }
-            {
-              driver.status !== "verified" && (
-                <div className="border border-red-500 bg-red-500 rounded-full p-1">
-                  <FaTimes className="w-4 h-4 text-white" />
-                </div>
-              )
-            }
-          </Tooltip>
-        </h1>
-
-        {/* Trip specific data */}
-        <div className="flex flex-wrap gap-2 items-stretch justify-between h-full">
-          <div className="flex flex-col">
-            <div className="flex text-sm text-gray-500 dark:text-gray-400 flex-col font-light h-full justify-center gap-1">
-              <span className="text-gray-600 dark:text-gray-400 text-xs bg-gray-200 dark:bg-gray-700 rounded-lg py-[0.15rem] px-1 flex gap-1 items-center w-fit">
-                <IoIosMail className="w-3 h-3 inline-block" />
-                <span className="text-gray-800 dark:text-gray-200 text-xs">
-                  {driver.email}
-                </span>
-              </span>
-              <span className="text-gray-600 dark:text-gray-400 text-xs bg-gray-200 dark:bg-gray-700 rounded-lg py-[0.15rem] px-1 flex gap-1 items-center w-fit">
-                <FaPhoneAlt className="w-3 h-3 inline-block" />
-                <span className="text-gray-800 dark:text-gray-200 text-xs">{driver.phone}</span>
-              </span>
-              <span className="text-gray-600 dark:text-gray-400 text-xs bg-gray-200 dark:bg-gray-700 rounded-lg py-[0.15rem] px-1 flex gap-1 items-center w-fit">
-                <FaIdCard className="w-3 h-3 inline-block" />
-                <span className="text-gray-800 dark:text-gray-200 text-xs">{driver.rut}</span>
+    <div className="flex flex-col gap-1 h-full">
+      <h1 className="text-md font-normal text-gray-700 dark:text-gray-300 flex flex-row gap-2 items-center">
+        {driver.name}
+        <Tooltip
+          style="auto"
+          content={
+            <div className="text-gray-600 dark:text-gray-400 text-sm flex gap-1 items-center w-fit font-light">
+              Estado:{" "}
+              <span className="text-gray-800 dark:text-gray-100 text-sm">
+                {driver.status}
               </span>
             </div>
-          </div>
-          <div className="flex flex-col">
-            <DriverValidations
-              driver={driver}
-              msg={(msg.pages as I18nRecord).transportValidationForm as I18nRecord}
-            />
+          }
+        >
+          {driver.status === "verified" && (
+            <div className="border border-green-500 bg-green-500 rounded-full p-1">
+              <FaCheck className="w-4 h-4 text-white" />
+            </div>
+          )}
+          {driver.status !== "verified" && (
+            <div className="border border-red-500 bg-red-500 rounded-full p-1">
+              <FaTimes className="w-4 h-4 text-white" />
+            </div>
+          )}
+        </Tooltip>
+      </h1>
+
+      {/* Trip specific data */}
+      <div className="flex flex-wrap gap-2 items-stretch h-full">
+        <div className="flex flex-col">
+          <div className="flex text-sm text-gray-500 dark:text-gray-400 flex-col font-light h-full justify-center gap-1">
+            <span className="text-gray-600 dark:text-gray-400 text-xs bg-gray-200 dark:bg-gray-700 rounded-lg py-[0.15rem] px-1 flex gap-1 items-center w-fit">
+              <IoIosMail className="w-3 h-3 inline-block" />
+              <span className="text-gray-800 dark:text-gray-200 text-xs">
+                {driver.email}
+              </span>
+            </span>
+            <span className="text-gray-600 dark:text-gray-400 text-xs bg-gray-200 dark:bg-gray-700 rounded-lg py-[0.15rem] px-1 flex gap-1 items-center w-fit">
+              <FaPhoneAlt className="w-3 h-3 inline-block" />
+              <span className="text-gray-800 dark:text-gray-200 text-xs">
+                {driver.phone}
+              </span>
+            </span>
+            <span className="text-gray-600 dark:text-gray-400 text-xs bg-gray-200 dark:bg-gray-700 rounded-lg py-[0.15rem] px-1 flex gap-1 items-center w-fit">
+              <FaIdCard className="w-3 h-3 inline-block" />
+              <span className="text-gray-800 dark:text-gray-200 text-xs">
+                {driver.rut}
+              </span>
+            </span>
           </div>
         </div>
+        <div className="flex flex-col">
+          <DriverValidations
+            driver={driver}
+            msg={
+              (msg.pages as I18nRecord).transportValidationForm as I18nRecord
+            }
+          />
+        </div>
       </div>
+    </div>
   );
 }

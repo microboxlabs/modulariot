6961f6e9ee98031cf093f6d242b00bde7b98e9bf
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-07-11 13:23:48 -0400

    Refactor ShippingPage component to enable session management and pass tenant ID to SseListener
    - Uncommented and utilized the auth function to manage user sessions.
    - Enhanced SseListener by passing tenant ID derived from the user session.

--- src/app/[lang]/(secured)/shippingv2/page.tsx
+++ src/app/[lang]/(secured)/shippingv2/page.tsx
@@ -1,5 +1,5 @@
 import "server-only";
-// import { auth } from "@/auth";
+import { auth } from "@/auth";
 // import { getUserTasks } from "@/features/common/providers/alfresco-api/alfresco-api.provider";
 import { getDictionary } from "@/features/i18n/i18n.service";
 import { I18nRecord, ParamsWithLang } from "@/features/i18n/i18n.service.types";
@@ -15,7 +15,7 @@ export default async function ShippingPage({
   params: { lang },
 }: ParamsWithLang) {
   const [, dictionary] = await getDictionary(lang);
-  // const session = await auth();
+  const session = await auth();
   // let tasks;
   try {
     // tasks = await getUserTasks(session!.user.ticket);
@@ -30,7 +30,7 @@ export default async function ShippingPage({
     // });
     return (
       <>
-        <SseListener dictionary={dictionary} />
+        <SseListener dictionary={dictionary} tenantId={session!.user.email} />
         <PageContent
           showFinishedTasks={false}
           kanbanBoards={staticData}

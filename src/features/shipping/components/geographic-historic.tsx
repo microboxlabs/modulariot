4ab9645bfbb79ac2b2b918ca4ff453efd6de3863
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2025-07-15 23:06:27 -0400

    problem fixed

--- src/features/shipping/components/geographic-historic.tsx
+++ src/features/shipping/components/geographic-historic.tsx
@@ -17,35 +17,7 @@ export function GeographicHistoric({
 }) {
   return (
     <div className="h-full w-full flex flex-col overflow-hidden gap-2">
-      <div className="h-2/3 rounded-lg border border-gray-200 dark:border-gray-700 overflow-hidden portrait:hidden">
-        <Geographic task={task} dictionary={dictionary} />
-      </div>
-      <div className="h-1/2 w-full overflow-hidden flex flex-col">
-        <div className="flex flex-row w-full">
-          <div
-            className={`w-1/2 border-l border-t border-gray-200 dark:border-gray-600 font-light text-gray-100 flex gap-2 justify-center align-middle py-1 rounded-tl-lg transition-all duration-300 ${selected_table === 0 ? "bg-blue-600 opacity-100 text-white" : "bg-gray-200 dark:bg-gray-800 opacity-50 cursor-pointer text-gray-800 dark:text-gray-100"}`}
-            onClick={() => setSelectedTable(0)}
-          >
-            <div className="w-4 h-full flex items-center justify-center">
-              <KanbanViewSwitcherIcon
-                className={`w-4 h-4 flex items-center justify-center ${selected_table === 0 ? "fill-gray-100" : "fill-gray-200"}`}
-              />
-            </div>
-            {
-              (
-                (
-                  (dictionary.pages as unknown as I18nRecord)
-                    .shipping as I18nRecord
-                ).kanban as I18nRecord
-              ).kanban_movements as unknown as string
-            }
-          </div>
-        </div>
-      </div>
-      <div className="h-1/3 w-full overflow-hidden flex flex-col portrait:h-full">
-        {/* Mobile selector */}
-        <HistoricLoads task={task} dictionary={dictionary} active={active} />
-      </div>
+      
     </div>
   );
 }

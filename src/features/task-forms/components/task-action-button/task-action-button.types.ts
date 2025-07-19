10eae7cc8f7187f463664f04b9aca06dc0fe91d1
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-07-18 21:38:55 -0400

    Refactor task actions to utilize new secondary transition logic
    Replaced the previous inline logic for generating task action options with a new function, getSecondaryTransitionIdV2, to streamline the handling of task outcomes. Updated Spanish labels for delivery outcomes to improve clarity and consistency across the application. This change enhances maintainability and readability of the task actions component.

--- src/features/task-forms/components/task-action-button/task-action-button.types.ts
+++ src/features/task-forms/components/task-action-button/task-action-button.types.ts
@@ -1,9 +1,13 @@
-import { TaskOutcome, TaskOutcomeV2 } from "../../services/form.service.types";
+import {
+  TaskOutcome,
+  TaskOutcomeDelivery,
+  TaskOutcomeV2,
+} from "../../services/form.service.types";
 
 export type TaskActionButtonProps = {
   label: string;
   taskId: string;
-  transitionId: TaskOutcome | TaskOutcomeV2;
+  transitionId: TaskOutcome | TaskOutcomeV2 | TaskOutcomeDelivery;
   fluid?: boolean;
   onClick: () => void;
 };

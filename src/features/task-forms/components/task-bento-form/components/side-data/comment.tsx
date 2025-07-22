2b49407d930ef2d7f7ee16eedbea56a305741bd2
khalzz <fearushbclear>
2025-07-22 10:02:09 -0400

    added no comment text

--- src/features/task-forms/components/task-bento-form/components/side-data/comment.tsx
+++ src/features/task-forms/components/task-bento-form/components/side-data/comment.tsx
@@ -11,15 +11,20 @@ export default function Comment({
   dict: I18nRecord;
 }) {
   const comments = getComments(task);
+
   return (
     <CustomCard
       title={(dict.comments as I18nRecord).title as string}
       subtitle={(dict.comments as I18nRecord).subtitle as string}
     >
       <div className="flex p-1 gap-1 flex-1 min-h-0 flex-col overflow-y-auto bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-700 rounded-lg text-gray-700 dark:text-gray-300">
-        {comments?.map((comment, index) => (
-          <div key={index}>{comment}</div>
-        ))}
+        {comments?.length > 0 ? (
+          comments?.map((comment, index) => <div key={index}>{comment}</div>)
+        ) : (
+          <div className="text-gray-500 dark:text-gray-400 w-full h-full flex items-center justify-center">
+            {(dict.comments as I18nRecord).no_comments as string}
+          </div>
+        )}
       </div>
     </CustomCard>
   );

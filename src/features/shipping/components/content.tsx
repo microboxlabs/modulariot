e10d14bc9c70a04d9aa69493c4c94038a746fc39
khalzz <fearushbclear>
2025-07-22 12:39:48 -0400

    starting working on client value filter not working

--- src/features/shipping/components/content.tsx
+++ src/features/shipping/components/content.tsx
@@ -69,6 +69,7 @@ export default function PageContent({
   const [list, setList] = useState<KanbanBoard[]>(kanbanBoards);
   const router = useRouter();
   const searchParams = useSearchParams();
+  console.log(searchParams);
 
   const [page, setPage] = useState(1);
   const [compactKanbanView, setCompactKanbanView] = useState(false);

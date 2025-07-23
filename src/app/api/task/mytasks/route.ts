11ad922a49054f225d6996c93dc3844b7f6d0b82
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2025-07-23 16:45:28 -0400

    build fix

--- src/app/api/task/mytasks/route.ts
+++ src/app/api/task/mytasks/route.ts
@@ -35,8 +35,6 @@ export async function GET(req: NextRequest) {
   const destination = url.searchParams.get("destination");
   const customer = url.searchParams.get("customer");
 
-  console.log(customerCode);
-
   let data: Record<string, KanbanBoard> = {};
   let total = 0;
 

5bb6c24887423e81bf1f0bf7324ddc7a22b87e27
khalzz <fearushbclear>
2025-07-24 10:25:59 -0400

    fixed search bar

--- src/features/layout/components/secured-navbar/searchbar/navegation_params.ts
+++ src/features/layout/components/secured-navbar/searchbar/navegation_params.ts
@@ -3,43 +3,26 @@
 import { I18nRecord } from "@/features/i18n/i18n.service.types";
 import { tr } from "@/features/i18n/tr.service";
 
+const finished = [
+  "service",
+  "licensePlate",
+  "driverId",
+  // "carrierName",
+  "carrierId",
+  "origin",
+  "destination",
+  "customer",
+];
+
 export function getNavegationParams(dict: I18nRecord) {
+  const finished_params = finished.map((param) => {
+    return {
+      label: tr(param, dict.searchbar as I18nRecord),
+      param,
+    };
+  });
+
   return {
-    finished: [
-      {
-        label: tr("service", dict.searchbar as I18nRecord),
-        param: "service",
-      },
-      {
-        label: tr("licensePlate", dict.searchbar as I18nRecord),
-        param: "licensePlate",
-      },
-      {
-        label: tr("driverId", dict.searchbar as I18nRecord),
-        param: "driverId",
-      },
-      {
-        label: tr("carrierId", dict.searchbar as I18nRecord),
-        param: "carrierId",
-      },
-      /*
-      {
-        label: tr("carrierName", (dict.searchbar as I18nRecord)),
-        param: "carrierName",
-      },
-      */
-      {
-        label: tr("origin", dict.searchbar as I18nRecord),
-        param: "origin",
-      },
-      {
-        label: tr("destination", dict.searchbar as I18nRecord),
-        param: "destination",
-      },
-      {
-        label: tr("customer", dict.searchbar as I18nRecord),
-        param: "customer",
-      },
-    ],
+    finished: finished_params,
   };
 }

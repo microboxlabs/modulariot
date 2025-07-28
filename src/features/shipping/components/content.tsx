c83a1bd6ddfe0f591f94674e97bdeb0a2df8ada8
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-07-28 17:22:08 -0400

    Adjust Kanban card dimensions for compact view; set width to 9rem and height to 3rem

--- src/features/shipping/components/content.tsx
+++ src/features/shipping/components/content.tsx
@@ -196,7 +196,8 @@ export default function PageContent({
                   <div
                     className={`mb-4 text-gray-900 dark:text-gray-300 text-center flex flex-col ${compactKanbanView ? "text-base font-semibold gap-2" : "h-[4.5rem] text-base font-semibold"}`}
                     style={{
-                      width: compactKanbanView ? "12rem" : "16rem",
+                      width: compactKanbanView ? "9rem" : "16rem",
+                      height: compactKanbanView ? "3rem" : "3rem",
                     }}
                   >
                     <div className="flex-1">

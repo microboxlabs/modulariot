2c5ed9daa0e2ca1dd2f37d8592ef3f7bff38057b
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-08-05 21:14:14 -0400

    Refactor DriverData and DriverValidations components to enhance driver information display and validation logic. Introduce ValidationIcon for better status representation and update translations for new validation types.

--- src/features/task-forms/components/task-bento-form/components/driver/validations.tsx
+++ src/features/task-forms/components/task-bento-form/components/driver/validations.tsx
@@ -6,36 +6,10 @@ import {
   ValidationStatus,
   ServiceValidationData,
 } from "./validations.types";
-import { FaCheck } from "react-icons/fa";
-import { TbExclamationMark } from "react-icons/tb";
 
 import CustomCard from "@/features/common/components/custom-card/custom-card";
 import { useGetValidation } from "@/features/common/providers/client-api.provider";
-
-// Validation status icons
-const ValidationIcon = ({ status }: { status: ValidationStatus }) => {
-  switch (status) {
-    case "ok":
-      return (
-        <div className="w-5 h-5 text-white bg-gray-400 rounded-full flex items-center justify-center p-1">
-          <FaCheck className="w-4 h-4" />
-        </div>
-      );
-    case "not_found":
-      return (
-        <div className="w-5 h-5 text-white bg-yellow-400 rounded-full flex items-center justify-center">
-          <TbExclamationMark className="w-4 h-4" />
-        </div>
-      );
-    case "error":
-    default:
-      return (
-        <div className="w-5 h-5 text-white bg-yellow-400 rounded-full flex items-center justify-center">
-          <TbExclamationMark className="w-4 h-4" />
-        </div>
-      );
-  }
-};
+import { ValidationIcon } from "./validation-icon";
 
 // Validation item component
 const ValidationItemComponent = ({

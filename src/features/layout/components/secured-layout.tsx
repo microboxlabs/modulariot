1b3194a1b8b8f52266c8c0ad6cff890ba743d292
Michel David <mdavid.cu@gmail.com>
2025-08-06 08:34:16 -0400

    Remove unused auth and SseListener imports from multiple secured pages to improve code clarity and maintainability.

--- src/features/layout/components/secured-layout.tsx
+++ src/features/layout/components/secured-layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { auth } from "@/auth";
 import { SidebarProvider } from "@/features/sidebar/context/sidebar-context";
 import { sidebarCookie } from "@/features/sidebar/services/sidebar-cookie.service";
 import { LayoutContent } from "@/features/layout/components/layout-content";
@@ -9,6 +10,7 @@ import { I18nRecord, ParamsWithLang } from "@/features/i18n/i18n.service.types";
 import { buildNavBarMessages } from "../utils/utils";
 import { SecuredSidebar } from "./secured-sidebar/secured-sidebar";
 import FooterSecuredLayout from "./footer-secured/footer-secured";
+import SseListener from "@/features/sse/components/sse-listener/sse-listener";
 
 export default async function SecuredLayout({
   children,
@@ -16,8 +18,10 @@ export default async function SecuredLayout({
 }: PropsWithChildren<ParamsWithLang>) {
   const [dict, dictionary] = await getDictionary(lang);
   const navBarMessages = buildNavBarMessages({ messages: dict });
+  const session = await auth();
   return (
     <SidebarProvider initialCollapsed={sidebarCookie.get().isCollapsed}>
+      <SseListener dictionary={dictionary} tenantId={session!.user.email} />
       <SecuredNavbar
         messages={navBarMessages}
         dict={dictionary as I18nRecord}

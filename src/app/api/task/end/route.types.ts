83a028fbbb8949e2d1319ce1be01da2cd9121a8a
Michel David <mdavid.cu@gmail.com>
2025-08-11 23:01:22 -0400

    refactor(task): remove unused reasonId and update task payload structure for comments

--- src/app/api/task/end/route.types.ts
+++ src/app/api/task/end/route.types.ts
@@ -10,8 +10,6 @@ export type EndTaskRequest = {
 export type UpdateTaskRequest = {
   prop_bpm_comment?: string;
   prop_mintral_shouldBuildManifest?: "true" | "false";
-  prop_wfship_sovosDigitalSignatureOutputReasonType?: string;
-  prop_wfship_transportValidationOutputReasonType?: string;
-  prop_wfship_missionControlValidationOutputReasonType?: string;
+  prop_mintral_commentPostTitle?: string;
   prop_cm_owner?: string;
 };

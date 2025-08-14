5a4af631551228932640a6ee914085d735b1f3a0
Michel David <mdavid.cu@gmail.com>
2025-08-14 15:55:55 -0400

    feat(auth): add "Continue with Microsoft" button text for sign-in forms in English and Spanish

--- src/features/auth/utils/utils.ts
+++ src/features/auth/utils/utils.ts
@@ -14,6 +14,7 @@ export function buildSignInFormMessages({
     buttonSubmitLabel: dict("pages.login.buttons.submit"),
     invalidCredentials: dict("pages.login.errors.invalidCredentials"),
     invalidFromData: dict("pages.login.errors.invalidFromData"),
+    buttonContinueWithMicrosoft: dict("pages.login.buttons.continueWithMicrosoft"),
   };
 }
 

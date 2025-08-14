a763f29189e6188e8a189c42ebb1724286aa65ed
khalzz <fearushbclear>
2025-08-14 16:47:35 -0400

    fixed text

--- src/features/task-forms/components/task-bento-form/components/side-data/conditions.tsx
+++ src/features/task-forms/components/task-bento-form/components/side-data/conditions.tsx
@@ -24,7 +24,7 @@ export default function Conditions({
   return (
     <CustomCard
       title={
-        (dict.bento as I18nRecord).conditions_present_in_the_trip as string
+        (dict.bento as I18nRecord).symptoms_present_in_the_trip as string
       }
       subtitle={null}
     >

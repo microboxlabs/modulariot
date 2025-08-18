cd15d89df99577e0a63e8737e8a3830764ad68a3
khalzz <fearushbclear>
2025-08-18 16:15:01 -0400

    Fixed data types

--- src/features/common/providers/alfresco-api/alfresco-api.provider.ts
+++ src/features/common/providers/alfresco-api/alfresco-api.provider.ts
@@ -490,7 +490,7 @@ export async function getTaskHistory(
   session: Session,
   taskId: string,
   active: boolean = true
-): Promise<any[]> {
+): Promise<TaskResponse> {
   const queryParams = new URLSearchParams({
     taskId,
     active: `${active}`,
@@ -502,7 +502,7 @@ export async function getTaskHistory(
     headers,
   });
 
-  return result as any[];
+  return result as TaskResponse;
 }
 
 export async function getTripLoads(

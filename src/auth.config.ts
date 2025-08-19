977ebfe5470bd724a67fb83c52ebbe0143e20726
Michel David <mdavid.cu@gmail.com>
2025-08-19 13:13:29 -0400

    refactor(auth): simplify authorized callback
    - Removed the asynchronous handling and simulated delay in the authorized callback to streamline the authentication process.
    - Enhanced the callback logic for better performance and clarity during user authorization.

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -21,7 +21,7 @@ export const authConfig = {
     signIn: "/app/sign-in",
   },
   callbacks: {
-    async authorized({ auth, request }) {
+    authorized({ auth, request }) {
       try {
       const nextUrl = new URL(request.nextUrl);
       
@@ -53,7 +53,6 @@ export const authConfig = {
         path: nextUrl.pathname,
         userId: auth?.user?.id 
       });
-      await new Promise((resolve) => setTimeout(resolve, 5000));
       return true;
       } catch (error) {
         authAuthzLogger.error("Error in authorized callback", { error });

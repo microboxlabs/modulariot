c2f2cd196e39521dbb66caebe89587484b0f6194
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-08-26 12:29:21 -0400

    refactor(validations): streamline validation logic and tooltip handling
    - Replaced repetitive validation status checks with a centralized mapping function to improve code maintainability.
    - Simplified tooltip message retrieval by removing unnecessary conditions, enhancing clarity in tooltip rendering.
    - Updated validation icon rendering for the "not_found" state to improve visual consistency.

--- src/features/task-forms/components/task-bento-form/components/driver/validation-icon.tsx
+++ src/features/task-forms/components/task-bento-form/components/driver/validation-icon.tsx
@@ -1,5 +1,5 @@
 import { FaCheck } from "react-icons/fa";
-import { TbExclamationMark, TbX } from "react-icons/tb";
+import { TbExclamationMark } from "react-icons/tb";
 import { ValidationStatus } from "./validations.types";
 
 // Validation status icons
@@ -25,15 +25,13 @@ export const ValidationIcon = ({
       );
     case "not_found":
       return (
-        <div className="w-5 h-5 text-white bg-yellow-400 rounded-full flex items-center justify-center">
-          <TbExclamationMark className="w-4 h-4" />
-        </div>
+        <div className="w-5 h-5 text-white bg-white rounded-full flex items-center justify-center border-2 border-gray-400"></div>
       );
     case "error":
     default:
       return (
         <div className="w-5 h-5 text-white bg-yellow-400 rounded-full flex items-center justify-center">
-          <TbX className="w-4 h-4" />
+          <TbExclamationMark className="w-4 h-4" />
         </div>
       );
   }

06ac6fbe4bd6b7a1b151e2f15d2185c88b3d982f
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-09-08 07:59:42 -0300

    feat(custom-card): enhance CustomCard to support badges
    - Added badges prop to CustomCard for displaying contextual information.
    - Introduced InformationBadge interface to define badge properties including text, color, and icon.
    - Updated TripInformation component to utilize badges for displaying origin and urgency status.

--- src/features/task-forms/components/task-bento-form/components/trip-information/trip-information.tsx
+++ src/features/task-forms/components/task-bento-form/components/trip-information/trip-information.tsx
@@ -4,6 +4,8 @@ import TripData from "./trip-data";
 // import TripVerifications from "./trip-verifications";
 import CustomCard from "@/features/common/components/custom-card/custom-card";
 import { tr } from "@/features/i18n/tr.service";
+import { HiExclamationCircle, HiOutlineClock } from "react-icons/hi";
+import { logError } from "@/lib/logger";
 
 export default function TripInformation({
   task,
@@ -14,11 +16,62 @@ export default function TripInformation({
   msg: I18nRecord;
   isLoading?: boolean;
 }) {
+  let originIsSitrans = null;
+  if (task.mintral_geofenceDestinationMetadata) {
+    try {
+      originIsSitrans = (
+        JSON.parse(
+          task.mintral_geofenceDestinationMetadata as string
+        ) as Record<string, boolean | undefined>
+      )["origin_is_sitrans"];
+    } catch (error) {
+      logError(error as Error);
+    }
+  }
+
+  let originIsSitransValue = "-";
+  if (originIsSitrans !== null) {
+    originIsSitransValue = originIsSitrans === true ? "internal" : "external";
+  }
+
+  const badges: Array<{
+    text: string;
+    color:
+      | "gray"
+      | "purple"
+      | "blue"
+      | "red"
+      | "green"
+      | "yellow"
+      | "pink"
+      | "indigo";
+    icon: any;
+  }> = [];
+
+  if (originIsSitrans !== null) {
+    badges.push({
+      text: (msg.bento as I18nRecord)[
+        originIsSitransValue as keyof I18nRecord
+      ] as string,
+      color: "gray" as const,
+      icon: HiOutlineClock,
+    });
+  }
+
+  if (task.mintral_priorityCode === "UR") {
+    badges.push({
+      text: (msg.bento as I18nRecord).urgency as string,
+      color: "purple" as const,
+      icon: HiExclamationCircle,
+    });
+  }
+
   return (
     <CustomCard
       title={tr("trip_information", msg.bento as I18nRecord)}
       /* subtitle={task.mintral_serviceCode + "-V"} */
       subtitle={(msg.bento as I18nRecord).trip as string}
+      badges={badges}
     >
       <div className="flex flex-col w-fit gap-2">
         <TripData

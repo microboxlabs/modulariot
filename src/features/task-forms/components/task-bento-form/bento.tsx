877764c88f7cb9d0e6808bda63ef74d9554007e5
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-09-26 14:34:06 -0300

    Refactor Bento component layout and comment out unused imports
    - Adjusted the layout of the Bento component by changing the column span for the Forum section and commenting out the Comment section to streamline the UI.
    - Removed unused imports for better code clarity and maintainability.

--- src/features/task-forms/components/task-bento-form/bento.tsx
+++ src/features/task-forms/components/task-bento-form/bento.tsx
@@ -13,8 +13,8 @@ import SymptomsCard from "./components/side-data/symptoms-card";
 import BentoHead from "./bento-head";
 // import Forum from "./components/forum/forum";
 import Forum from "./components/forum/forum";
-import Comment from "./components/side-data/comment";
-import { ExtendedTaskResponse } from "../task-form/task-form.types";
+/* import Comment from "./components/side-data/comment";
+import { ExtendedTaskResponse } from "../task-form/task-form.types"; */
 
 export default function Bento({
   lang,
@@ -118,16 +118,16 @@ export default function Bento({
         </div>
 
         {/* Forum */}
-        <div className="bg-white dark:bg-gray-800 rounded-lg border border-gray-300 dark:border-gray-700 overflow-hidden col-span-2 h-[400px]">
+        <div className="bg-white dark:bg-gray-800 rounded-lg border border-gray-300 dark:border-gray-700 overflow-hidden col-span-3 h-[400px]">
           <Forum dict={dict as I18nRecord} task={task} />
         </div>
         {/* Comment */}
-        <div className="bg-white dark:bg-gray-800 rounded-lg border border-gray-300 dark:border-gray-700 overflow-hidden col-span-1 h-[400px]">
+        {/* <div className="bg-white dark:bg-gray-800 rounded-lg border border-gray-300 dark:border-gray-700 overflow-hidden col-span-1 h-[400px]">
           <Comment
             task={task as ExtendedTaskResponse}
             dict={dict as I18nRecord}
           />
-        </div>
+        </div> */}
       </div>
       {/* Content */}
     </div>

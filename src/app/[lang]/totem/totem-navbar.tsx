2c1cf2d98bce8a44a877b725970ca1e51442b0c4
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-10-01 10:29:33 -0300

    refactor(navbar): replace logo image with AppLogo component
    - Removed direct image imports and replaced them with the new AppLogo component in both TotemNavbar and NavbarSignIn components for improved code reusability and consistency.

--- src/app/[lang]/totem/totem-navbar.tsx
+++ src/app/[lang]/totem/totem-navbar.tsx
@@ -1,8 +1,6 @@
 import { Navbar, NavbarBrand } from "flowbite-react";
-import Image from "next/image";
-// eslint-disable-next-line import/no-unresolved
-import logoImage from "@assets/logo-mintral-1.png";
 import CustomThemeToggle from "@/features/theme/components/CustomThemeToggle";
+import AppLogo from "@/features/common/components/app-logo/app-logo";
 
 export default function TotemNavbar() {
   return (
@@ -13,12 +11,7 @@ export default function TotemNavbar() {
           href="https://www.mintral.cl/"
         ></NavbarBrand>
 
-        <Image
-          src={logoImage}
-          className="mr-3"
-          alt="Coordinador Logo"
-          width={150}
-        />
+        <AppLogo />
         <CustomThemeToggle />
       </Navbar>
     </div>

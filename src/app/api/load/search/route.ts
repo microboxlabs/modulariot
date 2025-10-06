44d391ee1e630c0e3e1bfef90c3c60e05d1fb495
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-10-06 12:36:36 -0300

    Add API route for load search and define LoadSearchResponse type
    Created a new API route in `src/app/api/load/search/route.ts` to handle GET requests for load searches, including authentication and error handling. Introduced a new TypeScript interface `LoadSearchResponse` in `src/types/load.types.ts` to define the structure of the response data.

---
+++ src/app/api/load/search/route.ts
@@ -0,0 +1,47 @@
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+const SYMPTOMS_API_URL = `${process.env.OPENAPI_URL}/api/v1/pgrest/rpc/get_expedition_route`;
+const TOKEN = process.env.OPENAPI_TOKEN;
+import { LoadSearchResponse } from "@/types/load.types";
+
+export async function GET(request: Request) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.json({
+      status: 401,
+    });
+  }
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const loadId = searchParams.get("loadId");
+
+    const response = await fetch(SYMPTOMS_API_URL, {
+      method: "POST",
+      body: JSON.stringify({ p_expedition_id: +(loadId ?? "") }),
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const apiData = (await response.json()) as LoadSearchResponse;
+
+    return NextResponse.json(apiData);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        data: [],
+        status: 500,
+        message: error,
+      },
+      { status: 500 }
+    );
+  }
+}

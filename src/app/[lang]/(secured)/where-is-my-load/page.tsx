aeb5f5b01d50b0795943ae7b2f666005715a5343
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-10-06 12:24:58 -0300

    Refactor WheresMyLoadPage to utilize getDictionary correctly and clean up TimelineHeader usage

--- src/app/[lang]/(secured)/where-is-my-load/page.tsx
+++ src/app/[lang]/(secured)/where-is-my-load/page.tsx
@@ -1,18 +1,16 @@
-import { getDictionary } from "@/features/i18n/i18n.service";
 import { I18nRecord, ParamsWithLang } from "@/features/i18n/i18n.service.types";
 import Timeline from "@/features/where-is-my-load/timeline";
 import TimelineHeader from "@/features/where-is-my-load/components/header";
+import { getDictionary } from "@/features/i18n/i18n.service";
 
 export default async function WheresMyLoadPage({
   params: { lang },
 }: ParamsWithLang) {
   const [, dictionary] = await getDictionary(lang);
 
-  // Get the param load id from the params of the url
-
   return (
     <div className="h-full flex flex-col">
-      <TimelineHeader dict={dictionary as I18nRecord} />
+      <TimelineHeader />
       <div className="h-full w-full flex justify-center overflow-auto p-4">
         <Timeline dict={dictionary as I18nRecord} />
       </div>

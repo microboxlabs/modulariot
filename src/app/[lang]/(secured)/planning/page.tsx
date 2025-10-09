ae6dfdcd1d66699cd4fefc527404a6fa24f1bbb9
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2025-10-09 10:25:31 -0300

    refactor(planning): simplify PlanningPage component structure
    - Removed commented-out code and updated the function parameter type to `Readonly<ParamsWithLang>`.
    - Streamlined the data fetching process by directly calling `getStaticPlanningData()` instead of awaiting it.
    - Cleaned up the JSX structure for better readability while maintaining functionality.

--- src/app/[lang]/(secured)/planning/page.tsx
+++ src/app/[lang]/(secured)/planning/page.tsx
@@ -1,7 +1,6 @@
 import "server-only";
 import { auth } from "@/auth";
 import { getGroupsForPerson } from "@/features/common/providers/alfresco-api/alfresco-api.provider";
-// import { getUserTasks } from "@/features/common/providers/alfresco-api/alfresco-api.provider";
 import { getDictionary } from "@/features/i18n/i18n.service";
 import { I18nRecord, ParamsWithLang } from "@/features/i18n/i18n.service.types";
 import PageContent from "@/features/shipping/components/content";
@@ -9,27 +8,25 @@ import { getStaticPlanningData } from "@/features/shipping/services/data.service
 import { redirectWithLang } from "@/features/auth/services/navigation.service";
 export default async function PlanningPage({
   params: { lang },
-}: ParamsWithLang) {
+}: Readonly<ParamsWithLang>) {
   try {
     const [, dictionary] = await getDictionary(lang);
     const session = await auth();
     const userGroups = await getGroupsForPerson(session!);
-    const staticData = await getStaticPlanningData();
+    const staticData = getStaticPlanningData();
 
     return (
-      <>
-        <PageContent
-          showFinishedTasks={false}
-          showWorkflowTasks="planning"
-          kanbanBoards={staticData}
-          lang={lang}
-          dictionary={{
-            base: (dictionary.pages as I18nRecord)?.planning as I18nRecord,
-            general: dictionary,
-          }}
-          userGroups={userGroups}
-        />
-      </>
+      <PageContent
+        showFinishedTasks={false}
+        showWorkflowTasks="planning"
+        kanbanBoards={staticData}
+        lang={lang}
+        dictionary={{
+          base: (dictionary.pages as I18nRecord)?.planning as I18nRecord,
+          general: dictionary,
+        }}
+        userGroups={userGroups}
+      />
     );
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {

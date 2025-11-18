d063d824c7733d2e4f95cf55dcedf1d0aaba5471
Michel David <mdavid.cu@gmail.com>
2025-11-18 16:48:21 -0300

    update

--- src/features/task-forms/components/task-confirm-modal/task-confirm-modal.tsx
+++ src/features/task-forms/components/task-confirm-modal/task-confirm-modal.tsx
@@ -181,6 +181,7 @@ export default function TaskConfirmModal({
     } catch (error) {
       setIsProcessing(false);
       setError(error as ErrorWithAlfrescoError);
+      console.error(error);
     }
   }
 

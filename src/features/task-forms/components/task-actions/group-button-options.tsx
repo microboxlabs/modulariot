40f3904f98ee78582f5e3db49a23e6340239c407
Michel David <mdavid.cu@gmail.com>
2025-11-20 00:40:00 -0300

    Refactor dropdown components and improve localization
    - Introduced `twMerge` for better class management in layout and dropdown components.
    - Updated `SelectorDropdown` to use `DropdownItem` for consistency.
    - Enhanced localization by updating translation keys in `FileImages` and other components.
    - Made `transitionId` optional in `TaskActionButtonProps` for improved type safety.
    - Refactored `TaskActionDropdownButton` to utilize `TaskActionButton` for better reusability.

--- src/features/task-forms/components/task-actions/group-button-options.tsx
+++ src/features/task-forms/components/task-actions/group-button-options.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { Dropdown, DropdownItem } from "flowbite-react";
+import { Button, Dropdown, DropdownItem } from "flowbite-react";
 import { OtherOptionsProps } from "./other-options.types";
 import React, { useState, useEffect } from "react";
 import { TaskOutcome } from "../../services/form.service.types";
 import { TaskActionDropdownButton } from "./task-action-dropdown-button";
+import { twMerge } from "tailwind-merge";
+import { tr } from "@/features/i18n/tr.service";
+import { HiChevronUp } from "react-icons/hi";
+import TaskActionButton from "../task-action-button/task-action-button";
 
 interface GroupButtonOptionsProps extends OtherOptionsProps {
   otherOptions: Array<{
@@ -51,8 +55,40 @@ export default function GroupButtonOptions({
       <Dropdown
         placement="bottom-start"
         label={false}
+        theme={{
+          content: "z-20 w-full",
+          floating: {
+            base: "overflow-hidden rounded-lg z-20",
+            item: {
+              container: "w-full",
+            },
+          },
+        }}
         renderTrigger={() => (
-          <TaskActionDropdownButton isOpen={isOpen} dict={dict} />
+          <Button
+            color="alternative"
+            theme={{
+              base: twMerge(
+                "relative",
+                "flex items-center justify-center",
+                "!rounded-lg !rounded-r-none !border-l-1",
+                "text-center font-medium",
+                "focus:outline-none focus:ring-4",
+                "cursor-pointer",
+                "h-10 transition-all duration-100 z-20",
+                "gap-2 w-fit"
+              ),
+            }}
+          >
+            <div className="flex items-center gap-2">
+              <span className="lg:block hidden whitespace-nowrap">
+                {tr("outcome.moreOptions", dict)}
+              </span>
+              <HiChevronUp
+                className={`w-5 h-5 transition-transform ease-in-out duration-300 ${isOpen ? "rotate-180" : ""}`}
+              />
+            </div>
+          </Button>
         )}
       >
         {otherOptions.map(({ id, label, icon: Icon }) => (

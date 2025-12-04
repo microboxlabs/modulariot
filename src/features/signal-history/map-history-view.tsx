52afe3cbc8ecbbadc140ef7eab36809bdcbb781d
khalzz <fearushbclear>
2025-12-04 18:24:57 -0300

    added more

--- src/features/signal-history/map-history-view.tsx
+++ src/features/signal-history/map-history-view.tsx
@@ -8,9 +8,7 @@ export default function MapHistoryView({
   return (
     <div className="w-full h-full flex flex-col gap-4">
       {/* Map History View Content */}
-      <h1 className="text-2xl font-bold">
-        {(dict.mapHistoryView as any).title as string}
-      </h1>
+      AAAA
       {/* Additional components and logic can be added here */}
     </div>
   );

2b6cca3d31bb4f406de9a6c530aee557621f72be
Michel David <mdavid.cu@gmail.com>
2026-01-04 11:10:37 -0300

    Remove DriverVerificationCard, DriverVerifiedCard, DriverVerifiedCard types, and TransportValidationForm components as part of code cleanup

--- src/features/task-forms/components/driver-verified-card/driver-verified-card.types.ts
+++
@@ -1,9 +0,0 @@
-import { GetEntityInfoResponse } from "@/features/common/providers/microboxlabs-api/microboxlabs-api.types";
-import { TaskFormProps } from "../task-form/task-form.types";
-import { ServiceValidationResponse } from "@/features/common/providers/alfresco-api/alfresco-api.types";
-
-export type DriverVerifiedCardProps = {
-  entityInfo?: GetEntityInfoResponse;
-  serviceValidation?: ServiceValidationResponse;
-  enableActions?: boolean;
-} & TaskFormProps;

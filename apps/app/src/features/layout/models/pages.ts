94da0735da0fadc2db0dbed391ecb816e2b1710e
Oscar Daniel Torres Hernández <odtorres891118@gmail.com>
2026-03-05 09:59:31 -0300

    feat: conditionally include fleet management sidebar item based on environment variable
    The fleet management sidebar item is now included only if the NEXT_PUBLIC_ENABLE_FLEET_MANAGEMENT environment variable is set to "true". This change enhances the flexibility of the sidebar configuration.

--- apps/app/src/features/layout/models/pages.ts
+++ apps/app/src/features/layout/models/pages.ts
@@ -72,13 +72,17 @@ export const pages: SidebarItem[] = [
         : undefined,
     requiredGroups: [], // Public route
   },
-  {
-    href: "/fleet-management",
-    icon: TruckIcon,
-    label: "fleetManagement",
-    totals: {},
-    requiredGroups: [],
-  },
+  ...(process.env.NEXT_PUBLIC_ENABLE_FLEET_MANAGEMENT === "true"
+    ? [
+        {
+          href: "/fleet-management",
+          icon: TruckIcon,
+          label: "fleetManagement",
+          totals: {},
+          requiredGroups: [],
+        },
+      ]
+    : []),
   {
     icon: CalendarIcon,
     label: "calendar",
